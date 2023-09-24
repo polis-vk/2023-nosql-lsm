42afc626f6db9fefc8b64a81f9354e3b77863128
Victor Ekhmenin <vitekh2001@yandex.ru>
2023-09-24 15:59:02 +0300

    refactoring

--- src/main/java/ru/vk/itmo/test/ekhmeninvictor/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/test/ekhmeninvictor/InMemoryDao.java
@@ -14,29 +14,28 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> myFactory = new DaoFactoryImpl();
 
-    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> inMemoryDao =
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> cache =
             new ConcurrentSkipListMap<>(Comparator.comparing(myFactory::toString));
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
-        return inMemoryDao.get(key);
+        return cache.get(key);
     }
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
 
-        if (inMemoryDao.isEmpty()) {
+        if (cache.isEmpty()) {
             return Collections.emptyIterator();
         }
-        return inMemoryDao.subMap(
-                from == null ? inMemoryDao.firstKey() : from, true,
-                to == null ? inMemoryDao.lastKey() : to, to == null // игнорируем правую границу, если она задана и не игнорируем, если необходимо идти до конца коллекции
+        return cache.subMap(
+                from == null ? cache.firstKey() : from, true,
+                to == null ? cache.lastKey() : to, to == null
         ).values().iterator();
     }
 
-
     @Override
     public void upsert(Entry<MemorySegment> entry) {
-        inMemoryDao.put(entry.key(), entry);
+        cache.put(entry.key(), entry);
     }
 }
