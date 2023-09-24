6946baa9f593dc8b812792d9ffb6b0f7081e6c96
Andrey Svistukhin <a.svistukhin@just-ai.com>
2023-09-24 20:41:03 +0300

    removed empty iterator

--- src/main/java/ru/vk/itmo/svistukhinandrey/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/svistukhinandrey/InMemoryDao.java
@@ -6,23 +6,11 @@ import ru.vk.itmo.Entry;
 import java.lang.foreign.MemorySegment;
 import java.util.Iterator;
 import java.util.NavigableMap;
-import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final NavigableMap<MemorySegment, Entry<MemorySegment>> inMemoryStorage;
-    private static final Iterator<Entry<MemorySegment>> EMPTY_ITERATOR = new Iterator<>() {
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        @Override
-        public Entry<MemorySegment> next() {
-            throw new NoSuchElementException();
-        }
-    };
 
     public InMemoryDao() {
         inMemoryStorage = new ConcurrentSkipListMap<>(new MemorySegmentComparator());
@@ -35,21 +23,14 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        if (inMemoryStorage.isEmpty()) {
-            return EMPTY_ITERATOR;
-        }
-
         if (from == null && to == null) {
             return inMemoryStorage.values().iterator();
         }
 
-        MemorySegment localFrom = from;
-        MemorySegment localTo = to;
         boolean last = false;
+        MemorySegment localFrom = (from == null) ? inMemoryStorage.firstKey() : from;
+        MemorySegment localTo = to;
 
-        if (from == null) {
-            localFrom = inMemoryStorage.firstKey();
-        }
         if (to == null) {
             localTo = inMemoryStorage.lastKey();
             last = true;
