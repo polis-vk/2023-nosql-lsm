24f4eba78b1340b7594fde5e0102f875d48cac03
yulalenk <yulya.alenkova@mail.ru>
2023-09-24 19:02:48 +0300

    avoid of extra checks

--- src/main/java/ru/vk/itmo/alenkovayulya/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/alenkovayulya/InMemoryDao.java
@@ -5,14 +5,14 @@ import ru.vk.itmo.Entry;
 
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.NavigableMap;
+import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.Supplier;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
-    private final NavigableMap<MemorySegment, Entry<MemorySegment>> entries;
+    private final SortedMap<MemorySegment, Entry<MemorySegment>> entries;
 
     public InMemoryDao() {
         entries = new ConcurrentSkipListMap<>(this::compareSegments);
@@ -20,14 +20,17 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
+        return getIterator(() -> {
+            if (from != null && to != null) {
+                return entries.subMap(from, to);
+            } else if (from == null && to != null) {
+                return entries.headMap(to);
+            } else if (from != null) {
+                return entries.tailMap(from);
+            }
+            return entries;
+        });
 
-        if (entries.isEmpty()) {
-            return Collections.emptyIterator();
-        }
-
-        return entries.subMap(
-                from == null ? entries.firstKey() : from, true,
-                to == null ? entries.lastKey() : to, to == null).values().iterator();
     }
 
     @Override
@@ -53,6 +56,10 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         return Byte.compare(
                 memorySegment1.get(ValueLayout.JAVA_BYTE, mismatchOffset),
                 memorySegment2.get(ValueLayout.JAVA_BYTE, mismatchOffset));
+    }
+
+    private Iterator<Entry<MemorySegment>> getIterator(Supplier<SortedMap<MemorySegment, Entry<MemorySegment>>> map) {
+        return map.get().values().iterator();
     }
 
 }
