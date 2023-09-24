2dd197eef2b0ca72ffe431b9312cf9a2cf82907c
GenryEden <a926788@gmail.com>
2023-09-24 19:25:55 +0300

    store pointers to Entries, not to values, to make it faster while getting data

--- src/main/java/ru/vk/itmo/test/emelyanovvitaliy/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/test/emelyanovvitaliy/InMemoryDao.java
@@ -1,13 +1,11 @@
 package ru.vk.itmo.test.emelyanovvitaliy;
 
-import ru.vk.itmo.BaseEntry;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
 import java.lang.foreign.MemorySegment;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
@@ -20,47 +18,30 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         }
         return o1.asByteBuffer().compareTo(o2.asByteBuffer());
     };
-    private final ConcurrentSkipListMap<MemorySegment, MemorySegment> mappings = new ConcurrentSkipListMap<>(
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> mappings = new ConcurrentSkipListMap<>(
             comparator
     );
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
-        return new BaseEntry<>(key, mappings.get(key));
+        return mappings.get(key);
     }
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        Iterator<Map.Entry<MemorySegment, MemorySegment>> iterator = privateGet(from, to);
-        return new Iterator<>() {
-            @Override
-            public boolean hasNext() {
-                return iterator.hasNext();
-            }
-
-            @Override
-            public Entry<MemorySegment> next() {
-                Map.Entry<MemorySegment, MemorySegment> result = iterator.next();
-                return new BaseEntry<>(result.getKey(), result.getValue());
-            }
-        };
+        if (from == null && to == null) {
+            return mappings.values().iterator();
+        } else if (from == null) {
+            return mappings.headMap(to).values().iterator();
+        } else if (to == null) {
+            return mappings.headMap(from).values().iterator();
+        }
+        return mappings.subMap(from, to)
+                .sequencedValues().iterator();
     }
 
     @Override
     public void upsert(Entry<MemorySegment> entry) {
-        mappings.put(entry.key(), entry.value());
+        mappings.put(entry.key(), entry);
     }
-
-    private Iterator<Map.Entry<MemorySegment, MemorySegment>> privateGet(MemorySegment from, MemorySegment to) {
-        if (from == null && to == null) {
-            return mappings.entrySet().iterator();
-        } else if (from == null) {
-            return mappings.headMap(to).entrySet().iterator();
-        } else if (to == null) {
-            return mappings.headMap(from).entrySet().iterator();
-        }
-        return mappings.subMap(from, to)
-                .entrySet().iterator();
-    }
-
 }
