75485bfb0d587fa01265eda652df7d4bb1f75a4a
lollyhunter <sdm.01@mail.ru>
2023-09-25 12:38:01 +0300

    fixed factory map

--- src/main/java/ru/vk/itmo/test/smirnovdmitrii/MemorySegmentFactory.java
+++ src/main/java/ru/vk/itmo/test/smirnovdmitrii/MemorySegmentFactory.java
@@ -2,10 +2,6 @@ package ru.vk.itmo.test.smirnovdmitrii;
 
 import java.lang.foreign.MemorySegment;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 import ru.vk.itmo.smirnovdmitrii.InMemoryDao;
@@ -14,8 +10,6 @@ import ru.vk.itmo.test.DaoFactory;
 @DaoFactory
 public class MemorySegmentFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
 
-    final Map<String, MemorySegment> memorySegmentMap = new ConcurrentHashMap<>();
-
     @Override
     public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
         return new InMemoryDao();
@@ -23,20 +17,12 @@ public class MemorySegmentFactory implements DaoFactory.Factory<MemorySegment, E
 
     @Override
     public String toString(final MemorySegment memorySegment) {
-        if (memorySegment == null) {
-            return null;
-        }
-        memorySegment.getUtf8String(0);
-        return Charset.defaultCharset().decode(memorySegment.asByteBuffer()).toString();
+        return memorySegment == null ? null : Charset.defaultCharset().decode(memorySegment.asByteBuffer()).toString();
     }
 
     @Override
     public MemorySegment fromString(final String data) {
-        if (data == null) {
-            return null;
-        }
-
-        return memorySegmentMap.computeIfAbsent(data,  k -> MemorySegment.ofArray(k.getBytes(StandardCharsets.UTF_8)));
+        return data == null ? null : MemorySegment.ofArray(data.getBytes(Charset.defaultCharset()));
     }
 
     @Override
