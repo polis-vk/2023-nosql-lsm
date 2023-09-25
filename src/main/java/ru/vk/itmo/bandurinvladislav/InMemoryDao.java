7ef5470a6e832e5a80cbf7d353d6f7735991b75a
vbandurin7 <v.bandurin.w@mail.ru>
2023-09-25 21:27:14 +0300

    Completed stage 1: implemented Dao and DaoFactory.Factory

---
+++ src/main/java/ru/vk/itmo/bandurinvladislav/InMemoryDao.java
@@ -0,0 +1,50 @@
+package ru.vk.itmo.bandurinvladislav;
+
+import ru.vk.itmo.Dao;
+import ru.vk.itmo.Entry;
+
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+    private static final Comparator<MemorySegment> MEMORY_SEGMENT_COMPARATOR = (m1, m2) ->
+        Arrays.compare(m1.toArray(ValueLayout.JAVA_BYTE), m2.toArray(ValueLayout.JAVA_BYTE));
+
+    private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> inMemoryStorage;
+
+    public InMemoryDao() {
+        inMemoryStorage = new ConcurrentSkipListMap<>(MEMORY_SEGMENT_COMPARATOR);
+    }
+
+    @Override
+    public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
+        return getEntryIterator(from, to);
+    }
+
+    @Override
+    public Entry<MemorySegment> get(MemorySegment key) {
+        return inMemoryStorage.get(key);
+    }
+
+    @Override
+    public void upsert(Entry<MemorySegment> entry) {
+        inMemoryStorage.put(entry.key(), entry);
+    }
+
+    private Iterator<Entry<MemorySegment>> getEntryIterator(MemorySegment from, MemorySegment to) {
+        if (from == null && to == null) {
+            return inMemoryStorage.values().iterator();
+        } else if (from != null && to != null) {
+            return inMemoryStorage.subMap(from, to).values().iterator();
+        } else if (from != null) {
+            return inMemoryStorage.tailMap(from, true).values().iterator();
+        } else {
+            return inMemoryStorage.headMap(to, false).values().iterator();
+        }
+    }
+}
