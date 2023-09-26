caf1d03306c5900acec88eb43ad52b0c37eb17f1
Волков Никита Петрович <nikita.volkov0505@mail.ru>
2023-09-26 15:24:23 +0300

    Рефакторинг

--- src/main/java/ru/vk/itmo/test/volkovnikita/DaoFactoryImpl.java
+++ src/main/java/ru/vk/itmo/test/volkovnikita/DaoFactoryImpl.java
@@ -30,7 +30,4 @@ public class DaoFactoryImpl implements DaoFactory.Factory<MemorySegment, Entry<M
     public Entry<MemorySegment> fromBaseEntry(Entry<MemorySegment> baseEntry) {
         return baseEntry;
     }
-    class EmptyWhiteSpase {
-
-    }
 }
