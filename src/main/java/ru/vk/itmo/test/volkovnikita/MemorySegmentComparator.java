2ddc1b1d1eaa4df94f5988abcf44530b9abec5e4
Волков Никита Петрович <nikita.volkov0505@mail.ru>
2023-09-26 18:04:01 +0300

    Рефакторинг

--- src/main/java/ru/vk/itmo/test/volkovnikita/MemorySegmentComparator.java
+++ src/main/java/ru/vk/itmo/test/volkovnikita/MemorySegmentComparator.java
@@ -1,23 +1,25 @@
 package ru.vk.itmo.test.volkovnikita;
 
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.ValueLayout;
 import java.util.Comparator;
+
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 
 public class MemorySegmentComparator implements Comparator<MemorySegment> {
 
     @Override
     public int compare(MemorySegment o1, MemorySegment o2) {
         long mismatchOffSet = o1.mismatch(o2);
+        if (mismatchOffSet == o2.byteSize()) {
+            return 1;
+        }
+        if (mismatchOffSet == o1.byteSize()) {
+            return -1;
+        }
         if (mismatchOffSet == -1) {
             return 0;
         }
-        if (o1.byteSize() == mismatchOffSet) {
-            return -1;
-        }
-        if (o2.byteSize() == mismatchOffSet) {
-            return 1;
-        }
-        return Byte.compare(o1.get(ValueLayout.JAVA_BYTE, mismatchOffSet), o2.get(ValueLayout.JAVA_BYTE, mismatchOffSet));
+
+        return o1.get(JAVA_BYTE, mismatchOffSet) - o2.get(JAVA_BYTE, mismatchOffSet);
     }
 }
