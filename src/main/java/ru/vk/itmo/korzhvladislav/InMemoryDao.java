eec8f9b551ab1a43beff6687fd5ca069b7e51d15
sold666 <korch937@mail.ru>
2023-09-26 15:16:20 +0300

    add first hw

---
+++ src/main/java/ru/vk/itmo/korzhvladislav/InMemoryDao.java
@@ -0,0 +1,51 @@
+package ru.vk.itmo.korzhvladislav;
+
+import ru.vk.itmo.Dao;
+import ru.vk.itmo.Entry;
+
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+    private final Comparator<MemorySegment> compareMemorySegments = (segFirst, segSecond) -> {
+        if (segFirst.byteSize() != segSecond.byteSize()) {
+            return Long.compare(segFirst.byteSize(), segSecond.byteSize());
+        }
+        long offSetMismatch = segFirst.mismatch(segSecond);
+        if (offSetMismatch == -1) {
+            return 0;
+        }
+        return Byte.compare(segFirst.get(ValueLayout.JAVA_BYTE, offSetMismatch),
+                segSecond.get(ValueLayout.JAVA_BYTE, offSetMismatch));
+    };
+
+    private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> dataStore =
+            new ConcurrentSkipListMap<>(compareMemorySegments);
+
+    @Override
+    public Entry<MemorySegment> get(MemorySegment key) {
+        return dataStore.get(key);
+    }
+
+    @Override
+    public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
+        if (from == null && to == null) {
+            return dataStore.values().iterator();
+        } else if (from == null) {
+            return dataStore.headMap(to, false).values().iterator();
+        } else if (to == null) {
+            return dataStore.tailMap(from, true).values().iterator();
+        } else {
+            return dataStore.subMap(from, to).values().iterator();
+        }
+    }
+
+    @Override
+    public void upsert(Entry<MemorySegment> entry) {
+        dataStore.put(entry.key(), entry);
+    }
+}
