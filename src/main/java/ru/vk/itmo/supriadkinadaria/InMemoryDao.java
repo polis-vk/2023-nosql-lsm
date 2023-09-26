1ce3d5c98aea61ee5bdda5d2961cab2d2e098a3e
sudarina <darya.supryadkina@mail.ru>
2023-09-27 01:24:41 +0300

    change lambda to inner static class

--- src/main/java/ru/vk/itmo/supriadkinadaria/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/supriadkinadaria/InMemoryDao.java
@@ -4,6 +4,7 @@ import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
 import java.lang.foreign.MemorySegment;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -13,17 +14,7 @@ import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> storage =
-            new ConcurrentSkipListMap<>((o1, o2) -> {
-                long mismatch = o1.mismatch(o2);
-                if (mismatch == -1) {
-                    return 0;
-                } else if (o1.byteSize() == mismatch) {
-                    return -1;
-                } else if (o2.byteSize() == mismatch) {
-                    return 1;
-                }
-                return o1.get(JAVA_BYTE, mismatch) - o2.get(JAVA_BYTE, mismatch);
-            });
+            new ConcurrentSkipListMap<>(new InMemoryDaoComparator());
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
@@ -45,5 +36,20 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     @Override
     public void upsert(Entry<MemorySegment> entry) {
         storage.put(entry.key(), entry);
+    }
+
+    public static class InMemoryDaoComparator implements Comparator<MemorySegment> {
+        @Override
+        public int compare(MemorySegment o1, MemorySegment o2) {
+            long mismatch = o1.mismatch(o2);
+            if (mismatch == -1) {
+                return 0;
+            } else if (o1.byteSize() == mismatch) {
+                return -1;
+            } else if (o2.byteSize() == mismatch) {
+                return 1;
+            }
+            return o1.get(JAVA_BYTE, mismatch) - o2.get(JAVA_BYTE, mismatch);
+        }
     }
 }
