5e0ca80fc71af4316f015fbca4f3f399ecdc8220
atveritin <atveritin@cinimex.ru>
2023-09-27 22:48:11 +0300

    Update comparator.

--- src/main/java/ru/vk/itmo/tveritinalexandr/InMemoryDaoImpl.java
+++ src/main/java/ru/vk/itmo/tveritinalexandr/InMemoryDaoImpl.java
@@ -12,14 +12,11 @@ import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 public class InMemoryDaoImpl implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final SortedMap<MemorySegment, Entry<MemorySegment>> dataBase = new ConcurrentSkipListMap<>((o1, o2) -> {
-        // Не очень понял кой порядок элементов нам нужно поддерживать, изначально думал, что
-        // будет логтчно "", "b", "aa", но в тесте проверяется "", "aa", "b"
-        if (o1.byteSize() == 0) return -1;
-
-        if (o1.byteSize() != o2.byteSize()) return Long.compare(o2.byteSize(), o1.byteSize());
-
         var offset = o1.mismatch(o2);
+
         if (offset == -1) return 0;
+        if (offset == o1.byteSize()) return -1;
+        if (offset == o2.byteSize()) return 1;
         if (o1.get(JAVA_BYTE, offset) > o2.get(JAVA_BYTE, offset)) return 1;
         else return -1;
     });
