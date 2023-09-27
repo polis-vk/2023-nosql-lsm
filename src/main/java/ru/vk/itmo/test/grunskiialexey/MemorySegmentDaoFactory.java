b93c9240690fc649990fe9f8f49486be965a8937
MegaVerkruzo <robochat@mail.ru>
2023-09-27 22:40:27 +0300

    Update code

--- src/main/java/ru/vk/itmo/test/grunskiialexey/MemorySegmentDaoFactory.java
+++ src/main/java/ru/vk/itmo/test/grunskiialexey/MemorySegmentDaoFactory.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ConcurrentMap;
 
 @DaoFactory
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
-    private final ConcurrentMap<String, byte[]> fromStringDict = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, MemorySegment> fromStringDict = new ConcurrentHashMap<>();
 
     @Override
     public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
@@ -22,9 +22,13 @@ public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment
 
     @Override
     public String toString(MemorySegment memorySegment) {
+        if (memorySegment == null) {
+            return null;
+        }
+
         // I use .asByteBuffer() because some properties like byte[] array will be linked and not copied
         // Also array() - just giving a link to the byte[] array inside ByteBuffer
-        return memorySegment == null ? null : new String(memorySegment.asByteBuffer().array(), StandardCharsets.UTF_8);
+        return new String(memorySegment.asByteBuffer().array(), StandardCharsets.UTF_8);
     }
 
     @Override
@@ -33,8 +37,8 @@ public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment
             return null;
         }
 
-        fromStringDict.computeIfAbsent(data, s -> s.getBytes(StandardCharsets.UTF_8));
-        return MemorySegment.ofArray(fromStringDict.get(data));
+        fromStringDict.computeIfAbsent(data, s -> MemorySegment.ofArray(s.getBytes(StandardCharsets.UTF_8)));
+        return fromStringDict.get(data);
     }
 
     @Override
