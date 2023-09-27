165ffd00d74977264a4e19e6c85e0363e0e7f452
AndrewDanilin <andnilin@gmail.com>
2023-09-28 00:15:09 +0300

    fix codeclimate problems 2

--- src/main/java/ru/vk/itmo/Dao.java
+++ src/main/java/ru/vk/itmo/Dao.java
@@ -1,13 +1,10 @@
 package ru.vk.itmo;
 
-import ru.vk.itmo.danilinandrew.InMemoryDao;
 import ru.vk.itmo.danilinandrew.MemorySegmentComparator;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.ValueLayout;
-import java.util.Comparator;
 import java.util.Iterator;
 
 public interface Dao<D, E extends Entry<D>> extends Closeable {
@@ -39,25 +36,6 @@ public interface Dao<D, E extends Entry<D>> extends Closeable {
             return next;
         }
         return null;
-    }
-
-    default int compare(MemorySegment o1, MemorySegment o2) {
-        int offset = 0;
-
-        while (offset < o1.byteSize() && offset < o2.byteSize()) {
-            byte byte1 = o1.get(ValueLayout.JAVA_BYTE, offset);
-            byte byte2 = o2.get(ValueLayout.JAVA_BYTE, offset);
-
-            int compareRes = Byte.compare(byte1, byte2);
-
-            if (compareRes == 0) {
-                offset++;
-            } else {
-                return compareRes;
-            }
-        }
-
-        return Long.compare(o1.byteSize(), o2.byteSize());
     }
 
     /**
