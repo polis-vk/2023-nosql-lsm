bbdf3de38fbf0ba6fa0de8e1ca54bad6cee73e08
Andrey Timofeev <andrey.timofeev@corp.mail.ru>
2023-09-27 23:48:52 +0300

    fix fromAll

--- src/main/java/ru/vk/itmo/test/at/ATDao.java
+++ src/main/java/ru/vk/itmo/test/at/ATDao.java
@@ -4,6 +4,7 @@ import java.lang.foreign.MemorySegment;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NavigableMap;
+import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import ru.vk.itmo.BaseEntry;
@@ -16,15 +17,14 @@ import ru.vk.itmo.Dao;
 public class ATDao implements Dao<MemorySegment, BaseEntry<MemorySegment>> {
     NavigableMap<MemorySegment, BaseEntry<MemorySegment>> cache = new ConcurrentSkipListMap<>(Comparator.comparing(MemorySegment::asByteBuffer));
 
-    @Override
     public Iterator<BaseEntry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        if (from == null) {
-            if (to == null) {
-                return cache.values().iterator();
-            }
-            return cache.headMap(to).values().iterator();
+        SortedMap<MemorySegment, BaseEntry<MemorySegment>> map = from == null
+                ? cache
+                : cache.tailMap(from);
+        if (to != null) {
+            map = map.headMap(to);
         }
-        return cache.tailMap(from).headMap(to).values().iterator();
+        return map.values().iterator();
     }
 
     @Override
