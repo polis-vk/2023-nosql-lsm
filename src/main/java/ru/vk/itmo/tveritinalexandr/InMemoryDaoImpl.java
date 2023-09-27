d0c49a6b2ca2667433185f0fec08b4864615dda0
atveritin <atveritin@cinimex.ru>
2023-09-27 15:32:52 +0300

    New comparator for MemorySegment that uses MemorySegment.mismatch().

--- src/main/java/ru/vk/itmo/tveritinalexandr/InMemoryDaoImpl.java
+++ src/main/java/ru/vk/itmo/tveritinalexandr/InMemoryDaoImpl.java
@@ -4,24 +4,20 @@ import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
 import java.lang.foreign.MemorySegment;
-import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 
 public class InMemoryDaoImpl implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final SortedMap<MemorySegment, Entry<MemorySegment>> dataBase = new ConcurrentSkipListMap<>((o1, o2) -> {
-        ByteBuffer o1Buffer = o1.asByteBuffer().asReadOnlyBuffer();
-        ByteBuffer o2Buffer = o2.asByteBuffer().asReadOnlyBuffer();
-
         if (o1.byteSize() != o2.byteSize()) return Long.compare(o1.byteSize(), o2.byteSize());
 
-        while (o1Buffer.remaining() != 0) {
-            int cmp = Byte.compare(o1Buffer.get(), o2Buffer.get());
-            if (cmp != 0) return cmp;
-        }
-        return 0;
+        var offset = o1.mismatch(o2);
+        if (offset == -1) return 0;
+        if (o1.get(JAVA_BYTE, offset) > o2.get(JAVA_BYTE, offset)) return 1;
+        else return -1;
     });
 
     @Override
