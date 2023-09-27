f3e33e35f291edcb934b9ca72f4b599ca517f5fc
Шишигин Степан <sshishigin190101@gmail.com>
2023-09-27 16:12:02 +0300

    [fix] codestyle

--- src/main/java/ru/vk/itmo/shishiginstepan/InMemDaoImpl.java
+++ src/main/java/ru/vk/itmo/shishiginstepan/InMemDaoImpl.java
@@ -13,11 +13,12 @@ public class InMemDaoImpl implements Dao<MemorySegment, Entry<MemorySegment>> {
     ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> storage = new ConcurrentSkipListMap<>(
             (o1, o2) -> Arrays.compare(o1.toArray(ValueLayout.JAVA_BYTE), o2.toArray(ValueLayout.JAVA_BYTE))
     );
+
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
         if (to == null && from == null) {
             return this.storage.values().iterator();
-        } else if (to == null){
+        } else if (to == null) {
             return this.storage.tailMap(from).sequencedValues().iterator();
         } else if (from == null) {
             return this.storage.headMap(to).sequencedValues().iterator();
