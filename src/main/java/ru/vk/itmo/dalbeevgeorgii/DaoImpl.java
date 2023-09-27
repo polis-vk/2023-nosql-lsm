d1799c18337524d61b06aa299bf7935949fb62b6
goshadalbeev <55684784+goshadalbeev@users.noreply.github.com>
2023-09-27 23:16:17 +0300

    codestyle

--- src/main/java/ru/vk/itmo/dalbeevgeorgii/DaoImpl.java
+++ src/main/java/ru/vk/itmo/dalbeevgeorgii/DaoImpl.java
@@ -9,28 +9,32 @@ import java.util.Iterator;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class DaoImpl implements Dao<MemorySegment, Entry<MemorySegment>> {
-    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> map = new ConcurrentSkipListMap<>((segment1, segment2) -> {
-        long offset = segment1.mismatch(segment2);
-        if (offset == -1) {
-            return 0;
-        } else if (offset == segment1.byteSize()) {
-            return -1;
-        } else if (offset == segment2.byteSize()) {
-            return 1;
-        }
-        return Byte.compare(segment1.get(ValueLayout.JAVA_BYTE, offset), segment2.get(ValueLayout.JAVA_BYTE, offset));
-    });
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> map = new ConcurrentSkipListMap<>(
+            (segment1, segment2) -> {
+                long offset = segment1.mismatch(segment2);
+                if (offset == -1) {
+                    return 0;
+                } else if (offset == segment1.byteSize()) {
+                    return -1;
+                } else if (offset == segment2.byteSize()) {
+                    return 1;
+                }
+                return Byte.compare(
+                        segment1.get(ValueLayout.JAVA_BYTE, offset),
+                        segment2.get(ValueLayout.JAVA_BYTE, offset)
+                );
+            });
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        if (from != null && to != null) {
-            return map.subMap(from, to).values().iterator();
-        } else if (to != null) {
-            return map.headMap(to).values().iterator();
-        } else if (from != null) {
+        if (from == null && to == null) {
+            return map.values().iterator();
+        } else if (to == null) {
             return map.tailMap(from).values().iterator();
+        } else if (from == null) {
+            return map.headMap(to).values().iterator();
         }
-        return map.values().iterator();
+        return map.subMap(from, to).values().iterator();
     }
 
     @Override
