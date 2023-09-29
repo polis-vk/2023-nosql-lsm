b8c2fab38ffde872dcbd2325a663e730e1c3435b
vkononov <vkononov@skbkontur.ru>
2023-09-29 23:21:33 +0300

    Кононов Владимир, апдейт тестов

--- src/test/java/ru/vk/itmo/BasicTest.java
+++ src/test/java/ru/vk/itmo/BasicTest.java
@@ -42,6 +42,21 @@ public class BasicTest extends BaseTest {
     }
 
     @DaoTest
+    void testOrder2(Dao<String, Entry<String>> dao) {
+        dao.upsert(entry("aa", "aa"));
+        dao.upsert(entry("b", "b"));
+        dao.upsert(entry("", ""));
+
+        assertSame(
+                dao.all(),
+
+                entry("", ""),
+                entry("aa", "aa"),
+                entry("b", "b")
+        );
+    }
+
+    @DaoTest
     void testTree(Dao<String, Entry<String>> dao) {
         dao.upsert(entry("e", "f"));
         dao.upsert(entry("c", "d"));
