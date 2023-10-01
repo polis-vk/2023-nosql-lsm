f73e10e117ca9443e571c939606eae5d8cba3aa8
vitaliy.emelyanov <vitaliy.emelyanov@yadro.com>
2023-10-01 18:50:27 +0300

    another style fix

--- src/main/java/ru/vk/itmo/test/emelyanovvitaliy/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/test/emelyanovvitaliy/InMemoryDao.java
@@ -11,6 +11,7 @@ import java.lang.foreign.ValueLayout;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
@@ -20,9 +21,6 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import static java.nio.channels.FileChannel.MapMode.READ_ONLY;
 import static java.nio.channels.FileChannel.MapMode.READ_WRITE;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.READ;
-import static java.nio.file.StandardOpenOption.WRITE;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     public static final String FILENAME = "sstable.save";
@@ -75,10 +73,12 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     @Override
     public void flush() throws IOException {
         long size = 0;
+        Set<StandardOpenOption> openOptions =
+                Set.of(StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE);
         for (Entry<MemorySegment> entry : mappings.values()) {
             size += entry.value().byteSize() + entry.key().byteSize() + 2 * Long.BYTES;
         }
-        try (FileChannel fc = FileChannel.open(sstablePath, Set.of(CREATE, READ, WRITE));
+        try (FileChannel fc = FileChannel.open(sstablePath, openOptions);
              Arena arena = Arena.ofConfined()) {
             Collection<Entry<MemorySegment>> vals = mappings.values();
             MemorySegment mapped = fc.map(READ_WRITE, 0, size, arena);
@@ -101,7 +101,8 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
             return null;
         }
         String strKey = new String(key.toArray(ValueLayout.JAVA_BYTE), UTF_8);
-        try (FileChannel fc = FileChannel.open(sstablePath, Set.of(CREATE, READ))) {
+        Set<StandardOpenOption> openOptions = Set.of(StandardOpenOption.CREATE, StandardOpenOption.READ);
+        try (FileChannel fc = FileChannel.open(sstablePath, openOptions)) {
             MemorySegment mapped = fc.map(READ_ONLY, 0, fc.size(), Arena.ofAuto());
             long offset = 0;
             while (offset < fc.size()) {
