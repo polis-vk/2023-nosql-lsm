72b4fd6d834a69132ee1601bd0dc38c3df8023ce
axothy <globaltouareg@gmail.com>
2023-10-01 16:57:17 +0300

    Made sstable private final

--- src/main/java/ru/vk/itmo/chebotinalexandr/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/chebotinalexandr/InMemoryDao.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
-    private SSTable sortedStringTable;
+    private final SSTable sortedStringTable;
     private final SortedMap<MemorySegment, Entry<MemorySegment>> entries =
             new ConcurrentSkipListMap<>(InMemoryDao::comparator);
 
