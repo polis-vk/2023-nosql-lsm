d3df2c30fc12c307784601ad33cf5114ca02fb89
Elena Khodosova <evkhodosova@gmail.com>
2023-10-04 01:23:52 +0300

    [stage-2] add sstable to store data

--- src/main/java/ru/vk/itmo/khodosovaelena/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/khodosovaelena/InMemoryDao.java
@@ -1,10 +1,18 @@
 package ru.vk.itmo.khodosovaelena;
 
+import ru.vk.itmo.BaseEntry;
+import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
+import java.io.IOException;
+import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentNavigableMap;
@@ -13,11 +21,50 @@ import java.util.concurrent.ConcurrentSkipListMap;
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> memorySegmentEntries
             = new ConcurrentSkipListMap<>(new MemorySegmentComparator());
+    private final Arena arena;
+    private final Path basePath;
+
+    public InMemoryDao(Config config) {
+        basePath = config.basePath().resolve("sstable.txt");
+        arena = Arena.ofConfined();
+    }
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
-        return memorySegmentEntries.get(key);
+        if (memorySegmentEntries.containsKey(key)) {
+            return memorySegmentEntries.get(key);
+        }
+
+        if (basePath == null || !Files.exists(basePath)) {
+            return null;
+        }
+        MemorySegment endSegment = null;
+        try (FileChannel channel = FileChannel.open(basePath, StandardOpenOption.READ)) {
+            MemorySegment mappedSegment = channel.map(FileChannel.MapMode.READ_ONLY, 0, Files.size(basePath), Arena.ofShared());
+
+            long offset = 0L;
+
+            while (offset < mappedSegment.byteSize()) {
+                long size = mappedSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+                offset += Long.BYTES;
+                MemorySegment segmentKey = mappedSegment.asSlice(offset, size);
+                offset += size;
+                if (key.mismatch(segmentKey) == -1) {
+                    size = mappedSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+                    offset += Long.BYTES;
+                    endSegment = mappedSegment.asSlice(offset, size);
+                    offset += size;
+                }
+            }
+            if (endSegment != null) {
+                return new BaseEntry<>(key, endSegment);
+            }
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
     }
+
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
@@ -38,6 +85,51 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         memorySegmentEntries.put(entry.key(), entry);
     }
 
+    @Override
+    public void flush(){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close(){
+        if (!arena.scope().isAlive()) {
+            return;
+        }
+        saveState();
+    }
+
+    private void saveState() {
+        long writeOffset = 0;
+        try (FileChannel channel = FileChannel.open(basePath,
+                StandardOpenOption.READ,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING,
+                StandardOpenOption.CREATE);
+            Arena writeArena = Arena.ofConfined()) {
+            MemorySegment mappedSegment = channel.map(FileChannel.MapMode.READ_WRITE, 0, getMapSize(), writeArena);
+            for (Entry<MemorySegment> entry : memorySegmentEntries.values()) {
+                mappedSegment.set(ValueLayout.JAVA_LONG_UNALIGNED, writeOffset, entry.key().byteSize());
+                writeOffset += Long.BYTES;
+                MemorySegment.copy(entry.key(), 0, mappedSegment, writeOffset, entry.key().byteSize());
+                writeOffset += entry.key().byteSize();
+                mappedSegment.set(ValueLayout.JAVA_LONG_UNALIGNED, writeOffset, entry.value().byteSize());
+                writeOffset += Long.BYTES;
+                MemorySegment.copy(entry.value(), 0, mappedSegment, writeOffset, entry.value().byteSize());
+                writeOffset += entry.value().byteSize();
+            }
+            mappedSegment.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private long getMapSize() {
+        long mapSize = 0;
+        for (Entry<MemorySegment> entry : memorySegmentEntries.values()) {
+            mapSize += entry.key().byteSize() + entry.value().byteSize();
+        }
+        return mapSize + Long.BYTES * memorySegmentEntries.size() * 2L;
+    }
     public static class MemorySegmentComparator implements Comparator<MemorySegment> {
         @Override
         public int compare(MemorySegment segment1, MemorySegment segment2) {
--- src/main/java/ru/vk/itmo/test/khodosovaelena/MyFactory.java
+++ src/main/java/ru/vk/itmo/test/khodosovaelena/MyFactory.java
@@ -1,5 +1,6 @@
 package ru.vk.itmo.test.khodosovaelena;
 
+import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 import ru.vk.itmo.khodosovaelena.InMemoryDao;
@@ -9,11 +10,11 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
 
-@DaoFactory
+@DaoFactory(stage = 2)
 public class MyFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
     @Override
-    public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
-        return new InMemoryDao();
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) {
+        return new InMemoryDao(config);
     }
 
     @Override
