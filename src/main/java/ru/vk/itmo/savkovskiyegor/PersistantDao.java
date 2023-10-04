9ba1c384c72ed221fc48b4f6096ceaf3578ecfdd
savkovkii <savkovskiy@tutu.tech>
2023-10-05 00:10:21 +0300

    LAB-2 Add: sstable realisation

--- src/main/java/ru/vk/itmo/savkovskiyegor/PersistantDao.java
+++ src/main/java/ru/vk/itmo/savkovskiyegor/PersistantDao.java
@@ -1,16 +1,23 @@
 package ru.vk.itmo.savkovskiyegor;
 
+import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
-public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
-    private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> memoryTable =
-            new ConcurrentSkipListMap<>(new MemorySegmentComparator());
+public class PersistantDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+    private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> memoryTable;
+    private final SSTable ssTable;
+
+    public PersistantDao(Config config) throws IOException {
+        this.memoryTable = new ConcurrentSkipListMap<>(new MemorySegmentComparator());
+        this.ssTable = new SSTable(config);
+    }
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
@@ -31,11 +38,28 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
-        return memoryTable.get(key);
+        var res = memoryTable.get(key);
+        if (res == null) {
+            return ssTable.get(key);
+        }
+
+        return res;
     }
 
     @Override
     public void upsert(Entry<MemorySegment> entry) {
         memoryTable.put(entry.key(), entry);
+    }
+
+    @Override
+    public void flush() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (!memoryTable.isEmpty()) {
+            ssTable.save(memoryTable);
+        }
     }
 }
--- src/main/java/ru/vk/itmo/test/savkovskiyegor/FactoryImpl.java
+++ src/main/java/ru/vk/itmo/test/savkovskiyegor/FactoryImpl.java
@@ -1,20 +1,21 @@
 package ru.vk.itmo.test.savkovskiyegor;
 
+import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
-import ru.vk.itmo.savkovskiyegor.InMemoryDao;
+import ru.vk.itmo.savkovskiyegor.PersistantDao;
 import ru.vk.itmo.test.DaoFactory;
 
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
 
-@DaoFactory
-public class InMemoryFactoryImpl implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
-
+@DaoFactory(stage = 2)
+public class FactoryImpl implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
     @Override
-    public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
-        return new InMemoryDao();
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) throws IOException {
+        return new PersistantDao(config);
     }
 
     @Override
