7c3a4f308a8e526d5bf5e9a0102527a4032c68f1
MegaVerkruzo <robochat@mail.ru>
2023-10-04 23:46:55 +0300

    Passed tests

--- src/main/java/ru/vk/itmo/grunskiialexey/MemorySegmentDao.java
+++ src/main/java/ru/vk/itmo/grunskiialexey/MemorySegmentDao.java
@@ -3,17 +3,33 @@ package ru.vk.itmo.grunskiialexey;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class MemorySegmentDao implements Dao<MemorySegment, Entry<MemorySegment>> {
-    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> data =
-            new ConcurrentSkipListMap<>((o1, o2) ->
-                    Arrays.compare(o1.toArray(ValueLayout.JAVA_BYTE), o2.toArray(ValueLayout.JAVA_BYTE))
-            );
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> data = new ConcurrentSkipListMap<>((o1, o2) ->
+            Arrays.compare(o1.toArray(ValueLayout.JAVA_BYTE), o2.toArray(ValueLayout.JAVA_BYTE))
+    );
+    private final Path filePath;
+
+    public MemorySegmentDao() {
+        this.filePath = null;
+    }
+
+    public MemorySegmentDao(List<Entry<MemorySegment>> listEntries, Path filePath) {
+        listEntries.forEach(entry -> data.put(entry.key(), entry));
+        this.filePath = filePath;
+    }
+
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
@@ -36,5 +52,29 @@ public class MemorySegmentDao implements Dao<MemorySegment, Entry<MemorySegment>
     @Override
     public void upsert(Entry<MemorySegment> entry) {
         data.put(entry.key(), entry);
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (filePath == null) {
+            return;
+        }
+
+        if (Files.exists(filePath)) {
+            Files.delete(filePath);
+        }
+        Files.createDirectories(filePath.getParent());
+        Files.createFile(filePath);
+
+        try (FileOutputStream outputStream = new FileOutputStream(filePath.toFile())) {
+            for (Entry<MemorySegment> entry : data.values()) {
+                byte[] key = entry.key().toArray(ValueLayout.JAVA_BYTE);
+                byte[] value = entry.value().toArray(ValueLayout.JAVA_BYTE);
+                outputStream.write(ByteBuffer.allocate(4).putInt(key.length).array());
+                outputStream.write(key);
+                outputStream.write(ByteBuffer.allocate(4).putInt(value.length).array());
+                outputStream.write(value);
+            }
+        }
     }
 }
--- src/main/java/ru/vk/itmo/test/grunskiialexey/MemorySegmentDaoFactory.java
+++ src/main/java/ru/vk/itmo/test/grunskiialexey/MemorySegmentDaoFactory.java
@@ -1,21 +1,62 @@
 package ru.vk.itmo.test.grunskiialexey;
 
 import ru.vk.itmo.BaseEntry;
+import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 import ru.vk.itmo.grunskiialexey.MemorySegmentDao;
 import ru.vk.itmo.test.DaoFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-@DaoFactory
+@DaoFactory(stage = 2)
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
 
     @Override
     public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
         return new MemorySegmentDao();
+    }
+
+    @Override
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) throws IOException {
+        Path filePath = Paths.get(config.basePath().toString(), "file");
+        Files.createDirectories(filePath.getParent());
+        if (!Files.exists(filePath)) {
+            return new MemorySegmentDao(List.of(), filePath);
+        }
+
+        try (FileInputStream ch = new FileInputStream(filePath.toFile())) {
+            List<Entry<MemorySegment>> list = new ArrayList<>();
+            byte[] array = new byte[4];
+            while (ch.read(array) > 0) {
+                int lengthKey = ByteBuffer.wrap(array).getInt();
+                byte[] key = new byte[lengthKey];
+                ch.read(key);
+                ch.read(array);
+                int lengthValue = ByteBuffer.wrap(array).getInt();
+                byte[] value = new byte[lengthValue];
+                ch.read(value);
+                list.add(new BaseEntry<>(
+                        MemorySegment.ofArray(key),
+                        MemorySegment.ofArray(value)
+                ));
+            }
+
+            return new MemorySegmentDao(
+                    list,
+                    filePath
+            );
+        }
     }
 
     @Override
