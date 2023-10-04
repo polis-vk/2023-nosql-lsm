b1765033657ae812b61f6d746993a269d0e117d7
sold666 <korch937@mail.ru>
2023-10-04 10:17:21 +0300

    put comparator in separate class

--- src/main/java/ru/vk/itmo/korzhvladislav/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/korzhvladislav/InMemoryDao.java
@@ -4,28 +4,13 @@ import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.ValueLayout;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
-    private final Comparator<MemorySegment> compareMemorySegments = (segFirst, segSecond) -> {
-        long offSetMismatch = segFirst.mismatch(segSecond);
-        if (offSetMismatch == -1) {
-            return 0;
-        } else if (offSetMismatch == segFirst.byteSize()) {
-            return -1;
-        } else if (offSetMismatch == segSecond.byteSize()) {
-            return 1;
-        }
-        return Byte.compare(segFirst.getAtIndex(ValueLayout.JAVA_BYTE, offSetMismatch),
-                segSecond.getAtIndex(ValueLayout.JAVA_BYTE, offSetMismatch));
-    };
-
     private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> dataStore =
-            new ConcurrentSkipListMap<>(compareMemorySegments);
+            new ConcurrentSkipListMap<>(new MemorySegmentComparator());
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
