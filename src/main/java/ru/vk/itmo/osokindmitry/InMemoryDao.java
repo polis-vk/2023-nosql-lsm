89bc17d57b6c81e90bea5d846778a99a66d678fd
osokindm <osokin34@gmail.com>
2023-10-04 21:54:24 +0300

    hw 2

--- src/main/java/ru/vk/itmo/osokindmitry/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/osokindmitry/InMemoryDao.java
@@ -10,12 +10,11 @@ import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.ConcurrentSkipListMap;
 
+import static java.nio.channels.FileChannel.MapMode.READ_ONLY;
 import static java.nio.channels.FileChannel.MapMode.READ_WRITE;
 import static java.nio.file.StandardOpenOption.*;
 
@@ -23,55 +22,41 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final Comparator<MemorySegment> comparator = InMemoryDao::compare;
 
-
     private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> storage
             = new ConcurrentSkipListMap<>(comparator);
 
-    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> mappedStorage
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> cachedValues
             = new ConcurrentSkipListMap<>(comparator);
 
-    private FileChannel fc;
-    private OutputStream outputStream;
+    private final Arena arena;
     private final Path path;
-
-    private static int compare(MemorySegment segment1, MemorySegment segment2) {
-        long offset = segment1.mismatch(segment2);
-        if (offset == -1) {
-            return 0;
-        } else if (offset == segment1.byteSize()) {
-            return -1;
-        } else if (offset == segment2.byteSize()) {
-            return 1;
-        }
-        byte b1 = segment1.get(ValueLayout.JAVA_BYTE, offset);
-        byte b2 = segment2.get(ValueLayout.JAVA_BYTE, offset);
-        return Byte.compare(b1, b2);
-    }
-
+    private static final String FILE_NAME = "sstable.txt";
 
     public InMemoryDao(Config config) {
-        path = config.basePath();
-        Set<OpenOption> opts = Set.of(CREATE, READ, WRITE);
-        try (FileChannel fc = FileChannel.open(config.basePath(), opts)) {
-            if (fc.size() != 0) {
-                Arena arena = Arena.ofAuto();
-                MemorySegment mappedSegment = fc.map(READ_WRITE, 0, fc.size(), arena);
-                sliceSegment(mappedSegment);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        path = config.basePath().resolve(FILE_NAME);
+        arena = Arena.ofConfined();
     }
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
         Entry<MemorySegment> entry = storage.get(key);
+        // avoiding extra file operations by checking cached values
         if (entry == null) {
-            entry = mappedStorage.get(key);
+            entry = cachedValues.get(key);
         }
+        // if value is still null then searching in file
+        if (entry == null) {
+            try (FileChannel fc = FileChannel.open(path, Set.of(CREATE, READ))) {
+                if (fc.size() != 0) {
+                    MemorySegment mappedSegment = fc.map(READ_ONLY, 0, fc.size(), arena);
+                    entry = searchInSlice(mappedSegment, key);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         return entry;
-        // взять ключ из постоянного хранилища
-        // storageP.get(key);
     }
 
     @Override
@@ -97,62 +82,69 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     @Override
     public void close() {
-        storage.values().forEach((value) -> {
-            try {
-                Files.write(path, value.key().toArray(ValueLayout.JAVA_BYTE), CREATE, WRITE, APPEND);
-                Files.write(path, value.value().toArray(ValueLayout.JAVA_BYTE), CREATE, WRITE, APPEND);
+        try (FileChannel fc = FileChannel.open(path, Set.of(CREATE, READ, WRITE))) {
 
-            } catch (IOException e) {
-                e.printStackTrace();
+            long ssTableSize = Long.BYTES * 2L * storage.size();
+            for (Entry<MemorySegment> value : storage.values()) {
+                ssTableSize += value.key().byteSize() + value.value().byteSize();
             }
-        });
-        // memtable -> sstable
+
+            MemorySegment ssTable = fc.map(READ_WRITE, 0, ssTableSize, arena);
+            long offset = 0;
+
+            for (Entry<MemorySegment> value : storage.values()) {
+                ssTable.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, value.key().byteSize());
+                offset += Long.BYTES;
+                MemorySegment.copy(value.key(), 0, ssTable, offset, value.key().byteSize());
+                offset += value.key().byteSize();
+
+                ssTable.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, value.value().byteSize());
+                offset += Long.BYTES;
+                MemorySegment.copy(value.value(), 0, ssTable, offset, value.value().byteSize());
+                offset += value.value().byteSize();
+            }
+            arena.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void sliceSegment(MemorySegment segment) {
-//        long n = segment.get(ValueLayout.JAVA_LONG, 0);
-//        List<Long> sizes = new ArrayList<>();
-//        for (long i = 0; i < n; i++) {
-//            sizes.add(segment.get(ValueLayout.JAVA_LONG, ValueLayout.JAVA_LONG.byteSize() * i));
-//        }
-//        for (long i = 0; i < n; i++) {
-//            mappedStorage.put(segment.asSlice(sizes.get(i * 2)))
-//        }
-
+    private Entry<MemorySegment> searchInSlice(MemorySegment mappedSegment, MemorySegment key) {
         long offset = 0;
-        while (offset < segment.byteSize() - ValueLayout.JAVA_LONG.byteSize()) {
+        while (offset < mappedSegment.byteSize() - Long.BYTES) {
 
-            long size = segment.get(ValueLayout.JAVA_LONG, offset);
-            offset += ValueLayout.JAVA_LONG.byteSize();
-            MemorySegment key = segment.asSlice(offset, size);
+            long size = mappedSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+            offset += Long.BYTES;
+            MemorySegment slicedKey = mappedSegment.asSlice(offset, size);
             offset += size;
 
-            size = segment.get(ValueLayout.JAVA_LONG, offset);
-            offset += ValueLayout.JAVA_LONG.byteSize();
-            MemorySegment value = segment.asSlice(offset, size);
+            size = mappedSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+            offset += Long.BYTES;
+            MemorySegment slicedValue = mappedSegment.asSlice(offset, size);
             offset += size;
 
-            mappedStorage.put(key, new BaseEntry<>(key, value));
+            BaseEntry<MemorySegment> entry = new BaseEntry<>(slicedKey, slicedValue);
+            cachedValues.put(slicedKey, entry);
+            if (compare(key, slicedKey) == 0) {
+                return entry;
+            }
         }
-
+        return null;
     }
 
-    private static byte[] longToBytes(long l) {
-        byte[] result = new byte[Long.BYTES];
-        for (int i = Long.BYTES - 1; i >= 0; i--) {
-            result[i] = (byte) (l & 0xFF);
-            l >>= Byte.SIZE;
+    private static int compare(MemorySegment segment1, MemorySegment segment2) {
+        long offset = segment1.mismatch(segment2);
+        if (offset == -1) {
+            return 0;
+        } else if (offset == segment1.byteSize()) {
+            return -1;
+        } else if (offset == segment2.byteSize()) {
+            return 1;
         }
-        return result;
-    }
-
-    private static long bytesToLong(final byte[] b) {
-        long result = 0;
-        for (int i = 0; i < Long.BYTES; i++) {
-            result <<= Byte.SIZE;
-            result |= (b[i] & 0xFF);
-        }
-        return result;
+        byte b1 = segment1.get(ValueLayout.JAVA_BYTE, offset);
+        byte b2 = segment2.get(ValueLayout.JAVA_BYTE, offset);
+        return Byte.compare(b1, b2);
     }
 
 }
