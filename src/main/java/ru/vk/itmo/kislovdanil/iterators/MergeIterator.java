2605c5c4574a10b19199ce3b6267a9f13aa25a72
burprop <danilburzua@gmail.com>
2023-10-06 20:51:46 +0300

    MergeIterator implementation

--- src/main/java/ru/vk/itmo/kislovdanil/iterators/MergeIterator.java
+++ src/main/java/ru/vk/itmo/kislovdanil/iterators/MergeIterator.java
@@ -2,23 +2,19 @@ package ru.vk.itmo.kislovdanil.iterators;
 
 import ru.vk.itmo.BaseEntry;
 import ru.vk.itmo.Entry;
-import ru.vk.itmo.kislovdanil.iterators.DatabaseIterator;
 
 import java.lang.foreign.MemorySegment;
 import java.util.*;
 
 public class MergeIterator implements Iterator<Entry<MemorySegment>> {
-    private static final class IteratorAndValue {
-        private final DatabaseIterator iterator;
-        private final MemorySegment value;
+    private final NavigableMap<MemorySegment, IteratorAndValue> itemsPool;
 
-        public IteratorAndValue(DatabaseIterator iterator, MemorySegment value) {
-            this.iterator = iterator;
-            this.value = value;
+    public MergeIterator(List<DatabaseIterator> iterators, Comparator<MemorySegment> comp) {
+        this.itemsPool = new TreeMap<>(comp);
+        for (DatabaseIterator iter : iterators) {
+            handleEntry(iter);
         }
     }
-
-    private final NavigableMap<MemorySegment, IteratorAndValue> itemsPool;
 
     private void handleEntry(DatabaseIterator iter) {
         while (iter.hasNext()) {
@@ -32,14 +28,6 @@ public class MergeIterator implements Iterator<Entry<MemorySegment>> {
         }
     }
 
-    public MergeIterator(List<DatabaseIterator> iterators, Comparator<MemorySegment> comp) {
-        this.itemsPool = new TreeMap<>(comp);
-        for (DatabaseIterator iter : iterators) {
-            handleEntry(iter);
-        }
-    }
-
-
     @Override
     public boolean hasNext() {
         return !this.itemsPool.isEmpty();
@@ -52,5 +40,15 @@ public class MergeIterator implements Iterator<Entry<MemorySegment>> {
         itemsPool.remove(key);
         handleEntry(iteratorAndValue.iterator);
         return new BaseEntry<>(key, iteratorAndValue.value);
+    }
+
+    private static final class IteratorAndValue {
+        private final DatabaseIterator iterator;
+        private final MemorySegment value;
+
+        public IteratorAndValue(DatabaseIterator iterator, MemorySegment value) {
+            this.iterator = iterator;
+            this.value = value;
+        }
     }
 }
