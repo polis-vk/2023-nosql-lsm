efe173dd24d8300279607a1eb467e85a8e28bb5b
Andrey Timofeev <andrey.timofeev@corp.mail.ru>
2023-10-07 21:45:47 +0300

    test timeout

--- src/test/java/ru/vk/itmo/PersistentTest.java
+++ src/test/java/ru/vk/itmo/PersistentTest.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class PersistentTest extends BaseTest {
 
