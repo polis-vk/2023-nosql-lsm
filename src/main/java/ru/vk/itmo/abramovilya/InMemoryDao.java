2df562b812c6948ac71f3c38bd660c881c4f6514
IlyaAbramovv <abramov.0383@gmail.com>
2023-10-13 19:54:16 +0300

    Add some logic to hw3

--- src/main/java/ru/vk/itmo/abramovilya/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/abramovilya/InMemoryDao.java
@@ -17,17 +17,31 @@ import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Collectors;
 
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> map =
             new ConcurrentSkipListMap<>(InMemoryDao::compareMemorySegments);
     private final Path storagePath;
-    private final Arena arena = Arena.ofConfined();
+    private final Arena arena = Arena.ofShared();
     private long readOffset;
     private long writeOffset;
+    private final String sstableBaseName = "storage";
+    private final String indexBaseName = "index";
+    private final Path metaFilePath;
 
     public InMemoryDao(Config config) {
-        storagePath = config.basePath().resolve("storage");
+        storagePath = config.basePath();
+
+        try {
+            metaFilePath = storagePath.resolve("meta");
+            if (!Files.exists(metaFilePath)) {
+                Files.createFile(metaFilePath);
+            }
+            Files.writeString(metaFilePath, "0");
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     @Override
@@ -56,17 +70,30 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         if (value != null) {
             return value;
         }
-        return seekForValueInFile(key);
+
+        try {
+            int totalSStables = getTotalSStables();
+            for (int sstableNum = totalSStables; sstableNum >= 0; sstableNum--) {
+                Path currSStablePath = storagePath.resolve(sstableBaseName + sstableNum);
+                var foundEntry = seekForValueInFile(key, currSStablePath);
+                if (foundEntry != null) {
+                    return foundEntry;
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        return null;
     }
 
-    private Entry<MemorySegment> seekForValueInFile(MemorySegment key) {
-        if (!Files.exists(storagePath)) {
+    private Entry<MemorySegment> seekForValueInFile(MemorySegment key, Path filePath) {
+        if (!Files.exists(filePath)) {
             return null;
         }
         readOffset = 0;
-        try (FileChannel fc = FileChannel.open(storagePath, StandardOpenOption.READ)) {
-            MemorySegment mapped = fc.map(FileChannel.MapMode.READ_ONLY, 0, Files.size(storagePath), arena);
-            while (readOffset < Files.size(storagePath)) {
+        try (FileChannel fc = FileChannel.open(filePath, StandardOpenOption.READ)) {
+            MemorySegment mapped = fc.map(FileChannel.MapMode.READ_ONLY, 0, Files.size(filePath), arena);
+            while (readOffset < Files.size(filePath)) {
                 MemorySegment keySegment = getMemorySegment(mapped);
                 if (compareMemorySegments(key, keySegment) == 0) {
                     return new BaseEntry<>(key, getMemorySegment(mapped));
@@ -87,8 +114,14 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     }
 
     @Override
-    public void flush() {
-        throw new UnsupportedOperationException();
+    public void flush() throws IOException {
+        writeMapIntoFile();
+        incTotalSStablesAmount();
+    }
+
+    private void incTotalSStablesAmount() throws IOException {
+        int totalSStables = getTotalSStables();
+        Files.writeString(metaFilePath, String.valueOf(totalSStables + 1));
     }
 
     @Override
@@ -96,24 +129,41 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         if (arena.scope().isAlive()) {
             arena.close();
         }
-        writeMapIntoFile();
+        flush();
     }
 
     private void writeMapIntoFile() throws IOException {
+        int currSStableNum = getTotalSStables() + 1;
+        Path sstablePath = storagePath.resolve(sstableBaseName + currSStableNum);
+
         writeOffset = 0;
-        try (var channel = FileChannel.open(storagePath,
+        try (var storageChannel = FileChannel.open(sstablePath,
                 StandardOpenOption.READ,
                 StandardOpenOption.WRITE,
-                StandardOpenOption.TRUNCATE_EXISTING,
                 StandardOpenOption.CREATE);
-             var writeArena = Arena.ofConfined()) {
-            MemorySegment mapped = channel.map(FileChannel.MapMode.READ_WRITE, 0, calcMapByteSizeInFile(), writeArena);
+             var indexChannel = FileChannel.open(sstablePath,
+                     StandardOpenOption.READ,
+                     StandardOpenOption.WRITE,
+                     StandardOpenOption.CREATE);
+             var storageArena = Arena.ofConfined();
+             var indexArena = Arena.ofConfined()) {
+            MemorySegment mappedStorage = storageChannel.map(FileChannel.MapMode.READ_WRITE, 0, calcMapByteSizeInFile(), storageArena);
+            MemorySegment mappedIndex = indexChannel.map(FileChannel.MapMode.READ_WRITE, 0, calcIndexByteSizeInFile(), indexArena);
             for (var entry : map.values()) {
-                writeMemorySegment(entry.key(), mapped);
-                writeMemorySegment(entry.value(), mapped);
+                writeMemorySegment(entry.key(), mappedStorage);
+                writeMemorySegment(entry.value(), mappedStorage);
             }
-            mapped.load();
+            mappedStorage.load();
         }
+    }
+
+
+    private int getTotalSStables() throws IOException {
+        return Integer.parseInt(Files.readString(metaFilePath));
+//        return 0;
+    }
+    private long calcIndexByteSizeInFile() {
+        return map.keySet().stream().mapToLong(k -> k.byteSize() + Long.BYTES).sum();
     }
 
     private long calcMapByteSizeInFile() {
@@ -121,19 +171,24 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         for (var entry : map.values()) {
             size += 2 * Long.BYTES;
             size += entry.key().byteSize();
-            size += entry.value().byteSize();
+            if (entry.value() != null) {
+                size += entry.value().byteSize();
+            }
         }
         return size;
     }
 
     // Every memorySegment in file has the following structure:
     // 8 bytes - size, <size> bytes - value
+    // If memorySegment has the size of 0 bytes, then it means its value is DELETED
     private void writeMemorySegment(MemorySegment memorySegment, MemorySegment mapped) {
-        long msSize = memorySegment.byteSize();
-        mapped.set(ValueLayout.JAVA_LONG_UNALIGNED, writeOffset, msSize);
-        writeOffset += Long.BYTES;
-        MemorySegment.copy(memorySegment, 0, mapped, writeOffset, msSize);
-        writeOffset += msSize;
+        if (memorySegment != null) {
+            long msSize = memorySegment.byteSize();
+            mapped.set(ValueLayout.JAVA_LONG_UNALIGNED, writeOffset, msSize);
+            writeOffset += Long.BYTES;
+            MemorySegment.copy(memorySegment, 0, mapped, writeOffset, msSize);
+            writeOffset += msSize;
+        }
     }
 
     private static int compareMemorySegments(MemorySegment segment1, MemorySegment segment2) {
