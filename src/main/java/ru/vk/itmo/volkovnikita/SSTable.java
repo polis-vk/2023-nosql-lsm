0d25f7843ea374f41c022291500354a4220c006c
Волков Никита Петрович <nikita.volkov0505@mail.ru>
2023-10-17 00:22:50 +0300

    refactoring

--- src/main/java/ru/vk/itmo/volkovnikita/SSTable.java
+++ src/main/java/ru/vk/itmo/volkovnikita/SSTable.java
@@ -4,12 +4,14 @@ import ru.vk.itmo.BaseEntry;
 import ru.vk.itmo.Config;
 import ru.vk.itmo.Entry;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.NavigableMap;
@@ -18,16 +20,46 @@ public class SSTable {
 
     private final Path basePath;
     private static final String FILE_PATH = "SSTable";
-    private final FileChannel fc;
+    private final MemorySegment readSegment;
+    private final Arena arena;
 
     public SSTable(Config config) throws IOException {
         this.basePath = config.basePath().resolve(FILE_PATH);
-        fc = FileChannel.open(basePath, StandardOpenOption.READ);
+
+        arena = Arena.ofShared();
+
+        long size;
+        try {
+            size = Files.size(basePath);
+        } catch (NoSuchFileException e) {
+            readSegment = null;
+            return;
+        }
+
+        boolean created = false;
+        MemorySegment pageCurrent;
+        try (FileChannel fileChannel = FileChannel.open(basePath, StandardOpenOption.READ)) {
+            pageCurrent = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, size, arena);
+            created = true;
+        } catch (FileNotFoundException e) {
+            pageCurrent = null;
+        } finally {
+            if (!created) {
+                arena.close();
+            }
+        }
+
+        readSegment = pageCurrent;
     }
 
     public void saveMemoryData(NavigableMap<MemorySegment, Entry<MemorySegment>> memorySegmentEntries)
             throws IOException {
-        long offset = 0L;
+        if (!arena.scope().isAlive()) {
+            return;
+        }
+
+        arena.close();
+
         long mappedMemorySize =
                 memorySegmentEntries.values().stream().mapToLong(e -> e.key().byteSize() + e.value().byteSize()).sum()
                         + Long.BYTES * memorySegmentEntries.size() * 2L;
@@ -40,15 +72,20 @@ public class SSTable {
              Arena writeArena = Arena.ofConfined()) {
             MemorySegment seg = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, mappedMemorySize, writeArena);
 
+            long offset = 0L;
+
             for (Entry<MemorySegment> entry : memorySegmentEntries.values()) {
-                seg.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, entry.key().byteSize());
+                MemorySegment key = entry.key();
+                MemorySegment value = entry.value();
+
+                seg.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, key.byteSize());
                 offset += Long.BYTES;
-                seg.asSlice(offset).copyFrom(entry.key());
+                MemorySegment.copy(key, 0, seg, offset, key.byteSize());
                 offset += entry.key().byteSize();
 
-                seg.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, entry.value().byteSize());
+                seg.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, value.byteSize());
                 offset += Long.BYTES;
-                seg.asSlice(offset).copyFrom(entry.value());
+                MemorySegment.copy(value, 0, seg, offset, value.byteSize());
                 offset += entry.value().byteSize();
             }
         }
@@ -59,33 +96,28 @@ public class SSTable {
             return null;
         }
 
-        try {
-            long offset = 0L;
-            MemorySegment readSegment = fc.map(FileChannel.MapMode.READ_ONLY, 0, Files.size(basePath), Arena.ofAuto());
-            while (offset < readSegment.byteSize()) {
-                long keySize = readSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
-                offset += Long.BYTES;
+        long offset = 0L;
+        while (offset < readSegment.byteSize()) {
+            long keySize = readSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+            offset += Long.BYTES;
 
-                long valueSize = readSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset + keySize);
+            long valueSize = readSegment.get(ValueLayout.JAVA_LONG_UNALIGNED, offset + keySize);
 
-                if (keySize != key.byteSize()) {
-                    offset += keySize + valueSize + Long.BYTES;
-                    continue;
-                }
-
-                MemorySegment keySegment = readSegment.asSlice(offset, keySize);
-                offset += keySize + Long.BYTES;
-
-                if (key.mismatch(keySegment) == -1) {
-                    MemorySegment valueSegment = readSegment.asSlice(offset, valueSize);
-                    return new BaseEntry<>(keySegment, valueSegment);
-                }
-
-                offset += valueSize;
+            if (keySize != key.byteSize()) {
+                offset += keySize + valueSize + Long.BYTES;
+                continue;
             }
-            return null;
-        } catch (IOException e) {
-            return null;
+
+            MemorySegment keySegment = readSegment.asSlice(offset, keySize);
+            offset += keySize + Long.BYTES;
+
+            if (key.mismatch(keySegment) == -1) {
+                MemorySegment valueSegment = readSegment.asSlice(offset, valueSize);
+                return new BaseEntry<>(keySegment, valueSegment);
+            }
+
+            offset += valueSize;
         }
+        return null;
     }
 }
