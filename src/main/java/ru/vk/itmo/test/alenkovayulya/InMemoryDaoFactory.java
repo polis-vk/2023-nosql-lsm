c699c131cb6508bf727a46ef6dddbeff84ab96ce
yulalenk <yulya.alenkova@mail.ru>
2023-10-17 01:59:15 +0300

    fix for checkstyle after resolving conflicts

--- src/main/java/ru/vk/itmo/test/alenkovayulya/InMemoryDaoFactory.java
+++ src/main/java/ru/vk/itmo/test/alenkovayulya/InMemoryDaoFactory.java
@@ -1,6 +1,5 @@
 package ru.vk.itmo.test.alenkovayulya;
 
-
 import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
@@ -12,7 +11,6 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-
 
 @DaoFactory(stage = 2)
 public class InMemoryDaoFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
