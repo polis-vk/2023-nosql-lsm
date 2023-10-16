2d58c8ae79cee51ca8276aeb0995620d0bc87657
Trofimov Maksim <maks.trofimov.0303@mail.ru>
2023-10-16 23:36:25 +0300

    fix issues

--- src/main/java/ru/vk/itmo/trofimovmaxim/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/trofimovmaxim/InMemoryDao.java
@@ -5,23 +5,18 @@ import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.RandomAccessFile;
 import java.io.Serializable;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.nio.file.StandardOpenOption;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -32,10 +27,8 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
     private static final String FILENAME = "sstable";
 
     private final ConcurrentNavigableMap<MemorySegment, Entry<MemorySegment>> memTable;
-    private Config config;
-    private SsTable ssTable;
-
-    private final Logger logger = Logger.getLogger(InMemoryDao.class.getName());
+    private final Config config;
+    private final SsTable ssTable;
 
     private static final Comparator<MemorySegment> COMPARE_SEGMENT = (o1, o2) -> {
         if (o1 == null || o2 == null) {
@@ -57,14 +50,18 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     public InMemoryDao() {
         memTable = new ConcurrentSkipListMap<>(COMPARE_SEGMENT);
+        config = null;
+        ssTable = null;
     }
 
     public InMemoryDao(Config config) {
         this.config = config;
         memTable = new ConcurrentSkipListMap<>(COMPARE_SEGMENT);
-        ssTable = new SsTable(FILENAME);
-        if (ssTable.data == null || ssTable.offsetsTable == null) {
+        SsTable sstable = new SsTable(config.basePath(), FILENAME);
+        if (sstable.data == null || sstable.offsetsTable == null) {
             ssTable = null;
+        } else {
+            ssTable = sstable;
         }
     }
 
@@ -102,57 +99,81 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     @Override
     public void close() throws IOException {
-        if (config == null) {
+        if (config == null || ssTable == null || !ssTable.arena.scope().isAlive()) {
             return;
         }
-        ArrayList<SsTable.Offset> offsets = new ArrayList<>();
-        try (FileOutputStream stream = new FileOutputStream(
-                config.basePath().resolve(FILENAME + ".data").toFile());
-             FileOutputStream outputStream = new FileOutputStream(
-                     config.basePath().resolve(FILENAME + ".meta").toFile());
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
+        ssTable.arena.close();
 
-            for (Map.Entry<MemorySegment, Entry<MemorySegment>> entry : memTable.entrySet()) {
-                MemorySegment key = entry.getKey();
-                Entry<MemorySegment> val = entry.getValue();
+        long size = 0;
+        for (Map.Entry<MemorySegment, Entry<MemorySegment>> entry : memTable.entrySet()) {
+            size += entry.getKey().byteSize() + entry.getValue().key().byteSize() + entry.getValue().value().byteSize();
+        }
 
-                long prevOffset = 0;
-                if (!offsets.isEmpty()) {
-                    var last = offsets.getLast();
-                    prevOffset = last.keyOffset + last.keySize + last.val1Size + last.val2Size;
+        try (Arena writeArena = Arena.ofConfined()) {
+            try (FileChannel fileChannelData = FileChannel.open(config.basePath().resolve(FILENAME + ".data"),
+                    StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.TRUNCATE_EXISTING,
+                    StandardOpenOption.CREATE);
+                 FileChannel fileChannelMeta = FileChannel.open(config.basePath().resolve(FILENAME + ".meta"),
+                         StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.TRUNCATE_EXISTING,
+                         StandardOpenOption.CREATE)) {
+                MemorySegment pageData = fileChannelData.map(FileChannel.MapMode.READ_WRITE, 0, size, writeArena);
+                MemorySegment pageMeta = fileChannelMeta.map(FileChannel.MapMode.READ_WRITE, 0,
+                                                        4L * memTable.size() * Long.BYTES, writeArena);
+
+
+                long offsetData = 0;
+                long offsetMeta = 0;
+
+                for (Map.Entry<MemorySegment, Entry<MemorySegment>> entry : memTable.entrySet()) {
+                    MemorySegment key = entry.getKey();
+                    Entry<MemorySegment> val = entry.getValue();
+
+                    long keySize = key.byteSize();
+                    long val1Size = val.key().byteSize();
+                    long val2Size = val.value().byteSize();
+
+                    pageMeta.set(ValueLayout.JAVA_LONG_UNALIGNED, offsetMeta, offsetData);
+                    offsetMeta += Long.BYTES;
+                    pageMeta.set(ValueLayout.JAVA_LONG_UNALIGNED, offsetMeta, keySize);
+                    offsetMeta += Long.BYTES;
+                    pageMeta.set(ValueLayout.JAVA_LONG_UNALIGNED, offsetMeta, val1Size);
+                    offsetMeta += Long.BYTES;
+                    pageMeta.set(ValueLayout.JAVA_LONG_UNALIGNED, offsetMeta, val2Size);
+                    offsetMeta += Long.BYTES;
+
+                    MemorySegment.copy(key, 0, pageData, offsetData, keySize);
+                    offsetData += key.byteSize();
+                    MemorySegment.copy(val.key(), 0, pageData, offsetData, val1Size);
+                    offsetData += val.key().byteSize();
+                    MemorySegment.copy(val.value(), 0, pageData, offsetData, val2Size);
+                    offsetData += val.value().byteSize();
                 }
-
-                stream.write(toBytes(key));
-                stream.write(toBytes(val.key()));
-                stream.write(toBytes(val.value()));
-
-                offsets.add(
-                        new SsTable.Offset(prevOffset, key.byteSize(), val.key().byteSize(), val.value().byteSize()));
             }
-
-            objectOutputStream.writeObject(offsets);
         }
     }
 
-    private byte[] toBytes(MemorySegment segm) {
-        return segm.toArray(ValueLayout.OfByte.JAVA_BYTE);
-    }
+    private static class SsTable {
+        private MemorySegment data;
+        private MemorySegment offsetsTable;
+        private long size;
+        private final Arena arena;
 
-    private class SsTable {
-        MemorySegment data;
-        List<Offset> offsetsTable;
+        SsTable(Path basePath, String filename) {
+            Path pathToSsTable = basePath.resolve(filename + ".data");
+            Path pathToOffsetsTable = basePath.resolve(filename + ".meta");
 
-        SsTable(String filename) {
-            Path pathToSsTable = config.basePath().resolve(filename + ".data");
-            Path pathToOffsetsTable = config.basePath().resolve(filename + ".meta");
+            arena = Arena.ofShared();
 
-            try (RandomAccessFile randomAccessFile = new RandomAccessFile(pathToSsTable.toFile(), "r");
-                 FileChannel channel = randomAccessFile.getChannel();
-                 FileInputStream fileInputStream = new FileInputStream(pathToOffsetsTable.toFile());
-                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            Logger logger = Logger.getLogger(InMemoryDao.class.getName());
+            try (FileChannel channelData = FileChannel.open(pathToSsTable, StandardOpenOption.READ);
+                 FileChannel channelMeta = FileChannel.open(pathToOffsetsTable, StandardOpenOption.READ)) {
+                long sizeData = Files.size(pathToSsTable);
+                long sizeMeta = Files.size(pathToOffsetsTable);
 
-                data = channel.map(FileChannel.MapMode.READ_ONLY, 0, randomAccessFile.length(), Arena.global());
-                offsetsTable = (ArrayList<Offset>) objectInputStream.readObject();
+                this.size = (sizeData / Long.BYTES / 4L);
+
+                data = channelData.map(FileChannel.MapMode.READ_ONLY, 0, sizeData, arena);
+                offsetsTable = channelMeta.map(FileChannel.MapMode.READ_ONLY, 0, sizeMeta, arena);
             } catch (FileNotFoundException e) {
                 logger.log(Level.WARNING, String.format("File not found: %s", e));
             } catch (IOException e) {
@@ -160,35 +181,51 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
             } catch (Exception e) {
                 logger.log(Level.WARNING, String.format("Some exception: %s", e));
             }
+
+            if (data == null || offsetsTable == null) {
+                arena.close();
+            }
+        }
+
+        Offset getOffset(long index) {
+            long offset = 4L * index * Long.BYTES;
+            long keyOffset = offsetsTable.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+            long keySize = offsetsTable.get(ValueLayout.JAVA_LONG_UNALIGNED, offset + Long.BYTES);
+            long val1Size = offsetsTable.get(ValueLayout.JAVA_LONG_UNALIGNED, offset + 2L * Long.BYTES);
+            long val2Size = offsetsTable.get(ValueLayout.JAVA_LONG_UNALIGNED, offset + 3L * Long.BYTES);
+
+            return new Offset(keyOffset, keySize, val1Size, val2Size);
         }
 
         Entry<MemorySegment> get(MemorySegment key) {
-            for (Offset offset : offsetsTable) {
-                MemorySegment currentKey = MemorySegment.ofArray(read(offset.keyOffset, offset.keySize));
-                if (COMPARE_SEGMENT.compare(currentKey, key) == 0) {
+            long l = -1;
+            long r = size;
+            while (l < r - 1) {
+                long m = (l + r) / 2;
+                var offset = getOffset(m);
+                MemorySegment dataM = data.asSlice(offset.keyOffset, offset.keySize);
+                int cmp = COMPARE_SEGMENT.compare(dataM, key);
+                if (cmp == 0) {
                     return new BaseEntry<>(
-                            MemorySegment.ofArray(read(offset.keyOffset + offset.keySize, offset.val1Size)),
-                            MemorySegment.ofArray(
-                                    read(offset.keyOffset + offset.keySize + offset.val1Size, offset.val2Size))
+                            data.asSlice(offset.keyOffset + offset.keySize, offset.val1Size),
+                            data.asSlice(offset.keyOffset + offset.keySize + offset.val1Size, offset.val2Size)
                     );
                 }
+                if (cmp < 0) {
+                    l = m;
+                } else {
+                    r = m;
+                }
             }
+
             return null;
         }
 
-        byte[] read(long offset, long size) {
-            byte[] result = new byte[(int) size];
-            for (int i = 0; i < size; ++i) {
-                result[i] = data.get(ValueLayout.OfByte.JAVA_BYTE, offset + i);
-            }
-            return result;
-        }
-
         static class Offset implements Serializable {
-            long keyOffset;
-            long keySize;
-            long val1Size;
-            long val2Size;
+            private final long keyOffset;
+            private final long keySize;
+            private final long val1Size;
+            private final long val2Size;
 
             public Offset(long keyOffset, long keySize, long val1Size, long val2Size) {
                 this.keyOffset = keyOffset;
