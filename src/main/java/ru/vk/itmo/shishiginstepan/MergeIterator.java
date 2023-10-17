a0762d377c7d6b7951299585c689cb2dff695259
Шишигин Степан <sshishigin190101@gmail.com>
2023-10-17 22:16:13 +0300

    [feature] итератор мержащий все вложенные итераторы в порядке добавления итераторов

---
+++ src/main/java/ru/vk/itmo/shishiginstepan/MergeIterator.java
@@ -0,0 +1,112 @@
+package ru.vk.itmo.shishiginstepan;
+
+import ru.vk.itmo.Entry;
+
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class MergeIterator implements Iterator<Entry<MemorySegment>> {
+    private final List<IteratorWrapper> iterators;
+
+    private final Comparator<MemorySegment> keyComparator = new Comparator<MemorySegment>() {
+        @Override
+        public int compare(MemorySegment o1, MemorySegment o2) {
+            var mismatch = o1.mismatch(o2);
+            if (mismatch == -1) {
+                return 0;
+            }
+            if (mismatch == o1.byteSize()) {
+                return -1;
+            }
+            if (mismatch == o2.byteSize()) {
+                return 1;
+            }
+            byte b1 = o1.get(ValueLayout.JAVA_BYTE, mismatch);
+            byte b2 = o2.get(ValueLayout.JAVA_BYTE, mismatch);
+            return Byte.compare(b1, b2);
+        }
+    };
+    private static class IteratorWrapper implements Iterator<Entry<MemorySegment>>{
+        private Entry<MemorySegment> prefetched;
+        private final Iterator<Entry<MemorySegment>> iterator;
+
+        public IteratorWrapper(Iterator<Entry<MemorySegment>> iterator) {
+            this.iterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.iterator.hasNext() || this.prefetched != null;
+        }
+
+        @Override
+        public Entry<MemorySegment> next() {
+            if (this.prefetched == null) {
+                return this.iterator.next();
+            } else {
+                var toReturn = this.prefetched;
+                this.prefetched = null;
+                return toReturn;
+            }
+        }
+
+        public Entry<MemorySegment> peekNext() {
+            if (this.prefetched == null) {
+                this.prefetched = this.iterator.next();
+            }
+            return this.prefetched;
+        }
+
+        public void skip() {
+            if (this.prefetched != null) {
+                this.prefetched=null;
+                return;
+            }
+            this.iterator.next();
+        }
+    }
+
+    public MergeIterator(List<Iterator<Entry<MemorySegment>>> iterators){
+        // приоритет мержа будет определен порядком итераторов
+        this.iterators = new ArrayList<IteratorWrapper>();
+        iterators.forEach(iterator -> this.iterators.add(
+                new IteratorWrapper(iterator)
+        ));
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        for (var iterator: iterators) {
+            if (iterator.hasNext()) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Entry<MemorySegment> next() {
+        Entry<MemorySegment> nextEntry = null;
+        for (var iterator: this.iterators){
+            if (!iterator.hasNext()) continue;
+            nextEntry = iterator.peekNext();
+            break;
+        }
+        for (var iterator: this.iterators) {
+            if (!iterator.hasNext()) continue;
+            if (keyComparator.compare(nextEntry.key(), iterator.peekNext().key()) > 0) {
+                nextEntry = iterator.peekNext();
+            }
+        }
+        for (var iterator: this.iterators) {
+            if (!iterator.hasNext()) continue;
+            if (keyComparator.compare(nextEntry.key(), iterator.peekNext().key()) == 0) {
+                iterator.skip();
+            }
+        }
+        return nextEntry;
+    }
+}
