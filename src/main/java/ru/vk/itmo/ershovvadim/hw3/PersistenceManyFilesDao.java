e6723527adaaea64a43e0f35a60c86dd665abc6c
vadim01er <vadim.ershov.01@mail.ru>
2023-10-17 20:58:25 +0300

    HW 3 | Реализация третьего задания

---
+++ src/main/java/ru/vk/itmo/ershovvadim/hw3/PersistenceManyFilesDao.java
@@ -0,0 +1,166 @@
+package ru.vk.itmo.ershovvadim.hw3;
+
+import ru.vk.itmo.Config;
+import ru.vk.itmo.Dao;
+import ru.vk.itmo.Entry;
+
+import java.io.IOException;
+import java.lang.foreign.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Stream;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+public class PersistenceManyFilesDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+
+    static final String DATA = "data";
+    static final String INDEX = "index";
+    static final String SS_TABLE_DIR = "SSTable_";
+
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> db =
+            new ConcurrentSkipListMap<>(Utils::compare);
+
+    private final Path dir;
+    private final Arena arena;
+    private final List<SSTable> ssTables = new ArrayList<>();
+
+    public PersistenceManyFilesDao(Config config) throws IOException {
+        arena = Arena.ofConfined();
+
+        dir = config.basePath();
+        if (!Files.exists(dir)) {
+            return;
+        }
+
+        List<Path> ssTableDirs = getAllSSTableDir();
+
+        for (var ssTableDir : ssTableDirs) {
+            ssTables.add(new SSTable(ssTableDir, arena));
+        }
+    }
+
+    private List<Path> getAllSSTableDir() throws IOException {
+        List<Path> collect;
+        try (Stream<Path> fileStream = Files.list(dir)) {
+            collect = fileStream
+                    .filter(path -> path.getFileName().toString().startsWith(SS_TABLE_DIR))
+                    .toList();
+        }
+        return collect;
+    }
+
+
+    @Override
+    public Entry<MemorySegment> get(MemorySegment key) {
+        Iterator<Entry<MemorySegment>> iterator = get(key, null);
+        if (!iterator.hasNext()) {
+            return null;
+        }
+
+        var current = iterator.next();
+        if (key.mismatch(current.key()) == -1) {
+            return current;
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
+        List<PeekIterator<Entry<MemorySegment>>> iterators = new ArrayList<>();
+        iterators.add(new PeekIteratorImpl(getInMemory(from, to), Integer.MAX_VALUE));
+        for (SSTable ssTable : ssTables) {
+            iterators.add(ssTable.iterator(from, to));
+        }
+        return new MergeIterator(iterators);
+    }
+
+    public Iterator<Entry<MemorySegment>> getInMemory(MemorySegment from, MemorySegment to) {
+        if (from == null && to == null) {
+            return this.db.values().iterator();
+        } else if (to == null) {
+            return this.db.tailMap(from).values().iterator();
+        } else if (from == null) {
+            return this.db.headMap(to).values().iterator();
+        }
+        return this.db.subMap(from, to).values().iterator();
+    }
+
+    @Override
+    public void upsert(Entry<MemorySegment> entry) {
+        this.db.put(entry.key(), entry);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        if (arena.scope().isAlive()) {
+            arena.close();
+        }
+
+        if (db.isEmpty()) {
+            return;
+        }
+
+        try (var writeArena = Arena.ofConfined()) {
+            long indexSize = indexSize();
+            long tableSize = tableSize();
+            Path ssTableDir = createDir();
+
+            try (var fcTable = FileChannel.open(ssTableDir.resolve(DATA), TRUNCATE_EXISTING, CREATE, WRITE, READ);
+                 var fcIndex = FileChannel.open(ssTableDir.resolve(INDEX), TRUNCATE_EXISTING, CREATE, WRITE, READ)
+            ) {
+                MemorySegment msTable = fcTable.map(FileChannel.MapMode.READ_WRITE, 0, tableSize, writeArena);
+                MemorySegment msIndex = fcIndex.map(FileChannel.MapMode.READ_WRITE, 0, indexSize, writeArena);
+                long indexOffset = 0;
+                long tableOffset = 0;
+                for (Entry<MemorySegment> entry : db.values()) {
+                    msIndex.set(ValueLayout.JAVA_LONG_UNALIGNED, indexOffset, tableOffset);
+                    indexOffset += Long.BYTES;
+
+                    tableOffset += writeInFile(entry.key(), msTable, tableOffset);
+                    tableOffset += writeInFile(entry.value(), msTable, tableOffset);
+                }
+            }
+        }
+    }
+
+    private Path createDir() throws IOException {
+        Path ssTableDir = dir.resolve(SS_TABLE_DIR + ssTables.size());
+        Files.createDirectories(ssTableDir);
+        return ssTableDir;
+    }
+
+    private long tableSize() {
+        return db.values()
+                .stream()
+                .map(entry -> {
+                    long valueSize = (entry.value() == null) ? 0 : entry.value().byteSize();
+                    return 2L * Long.BYTES + entry.key().byteSize() + valueSize;
+                })
+                .reduce(0L, Long::sum);
+    }
+
+    private long indexSize() {
+        return (long) db.size() * Long.BYTES;
+    }
+
+    private long writeInFile(MemorySegment key, MemorySegment file, long fileOffset) {
+        if (key == null) {
+            file.set(ValueLayout.JAVA_LONG_UNALIGNED, fileOffset, -1);
+            return Long.BYTES;
+        }
+        long keySize = key.byteSize();
+        file.set(ValueLayout.JAVA_LONG_UNALIGNED, fileOffset, keySize);
+        MemorySegment.copy(key, 0, file, fileOffset + Long.BYTES, keySize);
+        return keySize + Long.BYTES;
+    }
+
+}
