0a875287847abc83dfd58eac21d302fbe3cfe628
Jenshen30 <mironkachmar@mail.ru>
2023-10-22 15:47:11 +0300

    extract code to SSTableController, fixed arenas usage

--- src/main/java/ru/vk/itmo/test/kachmareugene/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/test/kachmareugene/InMemoryDao.java
@@ -1,6 +1,5 @@
 package ru.vk.itmo.test.kachmareugene;
 
-import ru.vk.itmo.BaseEntry;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 
@@ -8,20 +7,13 @@ import java.io.IOException;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
-import java.nio.channels.FileChannel;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import static java.lang.Long.min;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.READ;
-import static java.nio.file.StandardOpenOption.WRITE;
-
 public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final Comparator<MemorySegment> memorySegmentComparatorImpl = new MemorySegmentComparator();
@@ -30,6 +22,7 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
 
     private final Path ssTablesDir;
     private static final String SS_TABLE_NAME = "ssTable.txt";
+    private final  Arena arena = Arena.ofShared();
 
     public InMemoryDao() {
         this.ssTablesDir = null;
@@ -56,26 +49,6 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         return dataSlice.values().iterator();
     }
 
-    private MemorySegment searchKeyInFile(MemorySegment mapped, MemorySegment key, long maxSize) {
-        long offset = 0L;
-        long keyByteSize;
-        long valueByteSize;
-
-        while (offset < maxSize) {
-            offset = alignmentBy(offset, Long.BYTES);
-            keyByteSize = mapped.get(ValueLayout.JAVA_LONG, offset);
-            offset += Long.BYTES;
-            valueByteSize = mapped.get(ValueLayout.JAVA_LONG, offset);
-            offset += Long.BYTES;
-
-            if (memorySegmentComparatorImpl.compare(key, mapped.asSlice(offset, keyByteSize)) == 0) {
-                return mapped.asSlice(offset + keyByteSize, valueByteSize);
-            }
-            offset += keyByteSize + valueByteSize;
-        }
-        return null;
-    }
-
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
         if (mp.containsKey(key)) {
@@ -86,12 +59,7 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
             return null;
         }
 
-        try (FileChannel channel = FileChannel.open(ssTablesDir.resolve(SS_TABLE_NAME), READ)) {
-            MemorySegment mapped = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size(), Arena.ofAuto());
-            return new BaseEntry<>(key, searchKeyInFile(mapped, key, channel.size()));
-        } catch (IOException e) {
-            return null;
-        }
+        return SSTablesController.getEntryFromSSTable(ssTablesDir, SS_TABLE_NAME, key, memorySegmentComparatorImpl, arena);
     }
 
     @Override
@@ -119,50 +87,8 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         }
     }
 
-    private long dumpSegmentSize(MemorySegment mapped, long size, long offset) {
-        long formattedOffset = alignmentBy(offset, Long.BYTES);
-        mapped.set(ValueLayout.JAVA_LONG, formattedOffset, size);
-        return formattedOffset + Long.BYTES;
-    }
-
-    private long dumpSegment(MemorySegment mapped, MemorySegment data, long offset) {
-        MemorySegment.copy(data, 0, mapped, offset, data.byteSize());
-        return offset + data.byteSize();
-    }
-
-    private long alignmentBy(long data, long by) {
-        return data % by == 0L ? data : data + (by - data % by);
-    }
-
     @Override
     public void close() throws IOException {
-        if (ssTablesDir == null) {
-            mp.clear();
-            return;
-        }
-
-        Set<OpenOption> options = Set.of(WRITE, READ, CREATE);
-        try (FileChannel channel = FileChannel.open(ssTablesDir.resolve(SS_TABLE_NAME), options)) {
-            long dataLenght = 0L;
-
-            for (var kv : mp.values()) {
-                dataLenght += alignmentBy(
-                        kv.key().byteSize() + kv.value().byteSize() + 2 * Long.BYTES,
-                        Long.BYTES);
-            }
-
-            long currOffset = 0L;
-            MemorySegment mappedSegment = channel.map(
-                    FileChannel.MapMode.READ_WRITE, currOffset, dataLenght, Arena.ofAuto());
-
-            for (var kv : mp.values()) {
-                currOffset = dumpSegmentSize(mappedSegment, kv.key().byteSize(), currOffset);
-                currOffset = dumpSegmentSize(mappedSegment, kv.value().byteSize(), currOffset);
-                currOffset = dumpSegment(mappedSegment, kv.key(), currOffset);
-                currOffset = dumpSegment(mappedSegment, kv.value(), currOffset);
-            }
-        } finally {
-            mp.clear();
-        }
+        SSTablesController.dump(ssTablesDir, SS_TABLE_NAME, mp, arena);
     }
 }
---
+++ src/main/java/ru/vk/itmo/test/kachmareugene/SSTablesController.java
@@ -0,0 +1,97 @@
+package ru.vk.itmo.test.kachmareugene;
+
+import ru.vk.itmo.BaseEntry;
+import ru.vk.itmo.Entry;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.nio.channels.FileChannel;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.SortedMap;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+public class SSTablesController {
+
+    private static long dumpSegmentSize(MemorySegment mapped, long size, long offset) {
+        mapped.set(ValueLayout.JAVA_LONG_UNALIGNED, offset, size);
+        return offset + Long.BYTES;
+    }
+
+    private static long dumpSegment(MemorySegment mapped, MemorySegment data, long offset) {
+        MemorySegment.copy(data, 0, mapped, offset, data.byteSize());
+        return offset + data.byteSize();
+    }
+
+    public static Entry<MemorySegment> getEntryFromSSTable(Path ssTablesDir, String SSTableName,
+                                                           MemorySegment key, Comparator<MemorySegment> comp,
+                                                           Arena arena) {
+        try (FileChannel channel = FileChannel.open(ssTablesDir.resolve(SSTableName), READ)) {
+            MemorySegment mapped = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size(), arena);
+            return new BaseEntry<>(key, SSTablesController.searchKeyInFile(mapped, key, channel.size(), comp));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static MemorySegment searchKeyInFile(MemorySegment mapped, MemorySegment key,
+                                                long maxSize, Comparator<MemorySegment> comp) {
+        long offset = 0L;
+        long keyByteSize;
+        long valueByteSize;
+
+        while (offset < maxSize) {
+            keyByteSize = mapped.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+            offset += Long.BYTES;
+            valueByteSize = mapped.get(ValueLayout.JAVA_LONG_UNALIGNED, offset);
+            offset += Long.BYTES;
+
+            if (comp.compare(key, mapped.asSlice(offset, keyByteSize)) == 0) {
+                return mapped.asSlice(offset + keyByteSize, valueByteSize);
+            }
+            offset += keyByteSize + valueByteSize;
+        }
+        return null;
+    }
+
+    public static void dump(Path ssTablesDir, String SStableName,
+                            SortedMap<MemorySegment, Entry<MemorySegment>> memTable, Arena sharedArena) throws IOException {
+        if (ssTablesDir == null) {
+            memTable.clear();
+            sharedArena.close();
+            return;
+        }
+
+        Set<OpenOption> options = Set.of(WRITE, READ, CREATE);
+        try (FileChannel channel = FileChannel.open(ssTablesDir.resolve(SStableName), options);
+             Arena arena  = Arena.ofConfined()) {
+            long dataLenght = 0L;
+
+            for (var kv : memTable.values()) {
+                dataLenght +=
+                        kv.key().byteSize() + kv.value().byteSize() + 2 * Long.BYTES;
+            }
+
+            long currOffset = 0L;
+            MemorySegment mappedSegment = channel.map(
+                    FileChannel.MapMode.READ_WRITE, currOffset, dataLenght, arena);
+
+            for (var kv : memTable.values()) {
+                currOffset = dumpSegmentSize(mappedSegment, kv.key().byteSize(), currOffset);
+                currOffset = dumpSegmentSize(mappedSegment, kv.value().byteSize(), currOffset);
+                currOffset = dumpSegment(mappedSegment, kv.key(), currOffset);
+                currOffset = dumpSegment(mappedSegment, kv.value(), currOffset);
+            }
+        } finally {
+            memTable.clear();
+            sharedArena.close();
+        }
+    }
+}
