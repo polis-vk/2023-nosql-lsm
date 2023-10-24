6bcc7fbcb2364a7e334a0f23b158ae56b396e504
Andrei_Smirnov <sandrew2003@gmail.com>
2023-10-24 17:29:31 +0300

    hw4 codeclimate fix2

--- src/main/java/ru/vk/itmo/smirnovandrew/DiskStorageHelper.java
+++ src/main/java/ru/vk/itmo/smirnovandrew/DiskStorageHelper.java
@@ -16,7 +16,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class DiskStorageHelper {
+public class DiskStorageHelper {
 
     protected DiskStorageHelper() {
         // Protected constructor prevents direct instantiation.
