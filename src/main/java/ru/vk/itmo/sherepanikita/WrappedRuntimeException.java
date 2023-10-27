ebb744ca3d74bf9b673f35441e60309c5c918f5e
beatHunteRcode <nikitasherepa@gmail.com>
2023-10-27 08:31:20 +0300

    HW4 fix

--- src/main/java/ru/vk/itmo/sherepanikita/WrappedRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package ru.vk.itmo.sherepanikita;
-
-public class WrappedRuntimeException extends RuntimeException {
-
-    public WrappedRuntimeException(Throwable t) {
-        super(t);
-    }
-}
