d8f8a4049ed40c098241fcfa1573ab4a45aec7e3
jaroslav <jaroslav.chernyshev@gmail.com>
2023-10-29 14:43:58 +0300

    +HW3, lots of copypasta from paschenkoalexandr

--- src/main/java/ru/vk/itmo/test/chernyshevyaroslav/InMemoryDaoFactory.java
+++ src/main/java/ru/vk/itmo/test/chernyshevyaroslav/InMemoryDaoFactory.java
@@ -1,22 +1,28 @@
 package ru.vk.itmo.test.chernyshevyaroslav;
 
+import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
 import ru.vk.itmo.chernyshevyaroslav.InMemoryDao;
 import ru.vk.itmo.test.DaoFactory;
+
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
 
-@DaoFactory
+@DaoFactory(stage = 3)
 public class InMemoryDaoFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
     @Override
-    public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
-        return new InMemoryDao();
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) throws IOException {
+        return new InMemoryDao(config);
     }
 
     @Override
     public String toString(MemorySegment memorySegment) {
+        if (memorySegment == null) {
+            return null;
+        }
         return new String(memorySegment.toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8);
     }
 
