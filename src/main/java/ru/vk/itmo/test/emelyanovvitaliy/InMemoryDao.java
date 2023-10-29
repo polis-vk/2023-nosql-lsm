e93f9809d5264ff34d16646ed99fef6208c7a6ab
vitaliy.emelyanov <vitaliy.emelyanov@yadro.com>
2023-10-29 19:10:15 +0300

    Part3 restyling after review
    Signed-off-by: vitaliy.emelyanov <vitaliy.emelyanov@yadro.com>

--- src/main/java/ru/vk/itmo/test/emelyanovvitaliy/InMemoryDao.java
+++ src/main/java/ru/vk/itmo/test/emelyanovvitaliy/InMemoryDao.java
@@ -14,7 +14,11 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ConcurrentSkipListSet;
@@ -47,8 +51,6 @@ public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
         sstablesPath = basePath;
         findSstables(basePath);
     }
-
-
 
     @Override
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
