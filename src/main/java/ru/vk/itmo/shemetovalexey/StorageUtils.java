ab2f26d8b2d4fab433e8602d84e81abeffd93f29
alexshemetov <alexshemetov0@gmail.com>
2023-11-01 01:15:48 +0300

    fix

--- src/main/java/ru/vk/itmo/shemetovalexey/StorageUtils.java
+++ src/main/java/ru/vk/itmo/shemetovalexey/StorageUtils.java
@@ -19,11 +19,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-abstract class StorageUtils {
+class StorageUtils {
     private static final String TMP_DIRECTORY_NAME = "tmp";
     private static final String INDEX_FILE_NAME = "index";
     private static final String IDX_EXTENSION = ".idx";
     private static final String TMP_EXTENSION = ".tmp";
+
+    private StorageUtils() {}
 
     public static void compact(Path path, Iterable<Entry<MemorySegment>> iterable) throws IOException {
         try {
