2111e541c37ddada20cdc80ca121a50a6e600115
Kononov Vladimir <vkononov@skbkontur.ru>
2023-11-01 15:35:15 +0300

    вернул код старого человека (почему-то он сменился из-за мержа)

--- src/main/java/ru/vk/itmo/test/pologovnikita/DaoFactoryImpl.java
+++ src/main/java/ru/vk/itmo/test/pologovnikita/DaoFactoryImpl.java
@@ -1,8 +1,9 @@
-package ru.vk.itmo.test.kononovvladimir;
+package ru.vk.itmo.test.pologovnikita;
 
 import ru.vk.itmo.Config;
 import ru.vk.itmo.Dao;
 import ru.vk.itmo.Entry;
+import ru.vk.itmo.pologovnikita.MemoryTable;
 import ru.vk.itmo.test.DaoFactory;
 
 import java.io.IOException;
@@ -10,23 +11,30 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
 
-@DaoFactory(stage = 3)
-public class FactoryImpl implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
+@DaoFactory(stage = 2)
+public class DaoFactoryImpl implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
+    @Override
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao() {
+        return new MemoryTable();
+    }
+
+    @Override
+    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) throws IOException {
+        return new MemoryTable(config);
+    }
+
     @Override
     public String toString(MemorySegment memorySegment) {
-        return memorySegment == null ? null
+        return memorySegment == null
+                ? null
                 : new String(memorySegment.toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8);
     }
 
     @Override
     public MemorySegment fromString(String data) {
-        return data == null ? null
+        return data == null
+                ? null
                 : MemorySegment.ofArray(data.getBytes(StandardCharsets.UTF_8));
-    }
-
-    @Override
-    public Dao<MemorySegment, Entry<MemorySegment>> createDao(Config config) throws IOException {
-        return new InMemoryDao(config);
     }
 
     @Override
