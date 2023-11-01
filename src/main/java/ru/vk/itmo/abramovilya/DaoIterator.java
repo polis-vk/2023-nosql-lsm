367aff2bef09555d2db3ac95f20adee5363d88ee
IlyaAbramovv <abramov.0383@gmail.com>
2023-11-01 11:36:03 +0300

    Satisfy codeclimate

--- src/main/java/ru/vk/itmo/abramovilya/DaoIterator.java
+++ src/main/java/ru/vk/itmo/abramovilya/DaoIterator.java
@@ -8,7 +8,11 @@ import ru.vk.itmo.abramovilya.table.TableEntry;
 
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
 
 class DaoIterator implements Iterator<Entry<MemorySegment>> {
     private final PriorityQueue<TableEntry> priorityQueue = new PriorityQueue<>();
--- src/main/java/ru/vk/itmo/abramovilya/StorageWriter.java
+++ src/main/java/ru/vk/itmo/abramovilya/StorageWriter.java
@@ -11,7 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.NavigableMap;
 
-class StorageWriter {
+final class StorageWriter {
     private StorageWriter() {
     }
 
