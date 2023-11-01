5edf72843e081508a9cfd0e922bf1e3e28400689
Kononov Vladimir <vkononov@skbkontur.ru>
2023-11-01 15:28:37 +0300

    удален свой старый код

--- src/main/java/ru/vk/itmo/test/kononovvladimir/KeySearcher.java
+++
@@ -1,9 +0,0 @@
-package ru.vk.itmo.test.kononovvladimir;
-
-import java.lang.foreign.MemorySegment;
-
-public class KeySearcher extends FileSearcher{
-    public KeySearcher(MemorySegment memorySegment, long size) {
-        super(memorySegment, size);
-    }
-}
