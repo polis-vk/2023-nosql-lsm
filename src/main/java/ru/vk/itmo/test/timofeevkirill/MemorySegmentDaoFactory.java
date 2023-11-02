02cd2c8a320ce960207ae29c689f184b8c77f060
Vadim Tsesko <incubos@users.noreply.github.com>
2023-11-02 15:31:35 +0300

    Fix unused imports

--- src/main/java/ru/vk/itmo/test/timofeevkirill/MemorySegmentDaoFactory.java
+++ src/main/java/ru/vk/itmo/test/timofeevkirill/MemorySegmentDaoFactory.java
@@ -6,11 +6,9 @@ import ru.vk.itmo.Entry;
 import ru.vk.itmo.test.DaoFactory;
 import ru.vk.itmo.timofeevkirill.InMemoryDao;
 
-import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 
 @DaoFactory(stage = 3)
 public class MemorySegmentDaoFactory implements DaoFactory.Factory<MemorySegment, Entry<MemorySegment>> {
