f8491f4471599b24ce3f8eff5cb9ed073c333c62
IsqK <plabolchar23@gmail.com>
2023-11-22 23:32:24 +0300

    fix codeclimate

--- src/main/java/ru/vk/itmo/kovalevigor/DaoImpl.java
+++ src/main/java/ru/vk/itmo/kovalevigor/DaoImpl.java
@@ -19,7 +19,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Level;
