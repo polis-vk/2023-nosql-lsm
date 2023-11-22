3fb14fb32b5a565b1e0cf1a8dfc2bb9607d4ef0b
lollyhunter <sdm.01@mail.ru>
2023-11-22 22:18:50 +0300

    a bit fair

--- src/main/java/ru/vk/itmo/smirnovdmitrii/inmemory/SkipListMemtable.java
+++ src/main/java/ru/vk/itmo/smirnovdmitrii/inmemory/SkipListMemtable.java
@@ -18,7 +18,7 @@ public class SkipListMemtable implements Memtable {
     public final Comparator<MemorySegment> comparator = new MemorySegmentComparator();
     private final SortedMap<MemorySegment, Entry<MemorySegment>> storage = new ConcurrentSkipListMap<>(comparator);
     private final AtomicLong currentSize = new AtomicLong(0);
-    private final ReadWriteLock lock = new ReentrantReadWriteLock(false);
+    private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
     private final Lock readLock = lock.readLock();
     private final Lock writeLock = lock.writeLock();
 
