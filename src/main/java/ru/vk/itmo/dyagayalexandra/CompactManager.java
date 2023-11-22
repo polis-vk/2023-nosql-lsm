f9098a5c55ea613aaef44cb6bb6d7b440a205cf5
Dyagay Alexandra <dyagaay@gmail.com>
2023-11-22 12:03:37 -0800

    codeclimate

--- src/main/java/ru/vk/itmo/dyagayalexandra/CompactManager.java
+++ src/main/java/ru/vk/itmo/dyagayalexandra/CompactManager.java
@@ -56,6 +56,7 @@ public class CompactManager {
             if (currentItem.value() != null) {
                 tableSize += currentItem.value().byteSize();
             }
+
             storageSize++;
         }
 
