bb2445e9ee9f96c87e09191227af57f4f08f8126
burprop <danilburzua@gmail.com>
2023-11-22 20:42:53 +0300

    Locks realisation (instead of beautiful lock-free :( )

--- src/main/java/ru/vk/itmo/kislovdanil/MemTable.java
+++ src/main/java/ru/vk/itmo/kislovdanil/MemTable.java
@@ -5,18 +5,36 @@ import ru.vk.itmo.Entry;
 import java.lang.foreign.MemorySegment;
 import java.util.Comparator;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 /* Basically, ConcurrentSkipList with counter of threads that putting value in it.
     Necessary for preventing data loss while flushing.
  */
 public class MemTable {
-    public ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> storage;
+    private final ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> storage;
+    private final long threshold;
+    private final AtomicLong size = new AtomicLong(0);
 
-    public MemTable(Comparator<MemorySegment> comparator) {
-        this.storage = new ConcurrentSkipListMap<>(comparator);
+    private static long getEntrySize(Entry<MemorySegment> entry) {
+        long valueSize = entry.value() == null ? 0 : entry.value().byteSize();
+        return valueSize + entry.key().byteSize();
     }
 
-    public Entry<MemorySegment> put(Entry<MemorySegment> entry) {
-        return storage.put(entry.key(), entry);
+    public MemTable(Comparator<MemorySegment> comparator, long threshold) {
+        this.storage = new ConcurrentSkipListMap<>(comparator);
+        this.threshold = threshold;
+    }
+
+    public boolean put(Entry<MemorySegment> entry) {
+        long entrySize = getEntrySize(entry);
+        if (size.addAndGet(entrySize) - entrySize > threshold) {
+            return false;
+        }
+        storage.put(entry.key(), entry);
+        return true;
+    }
+
+    public ConcurrentSkipListMap<MemorySegment, Entry<MemorySegment>> getStorage() {
+        return storage;
     }
 }
