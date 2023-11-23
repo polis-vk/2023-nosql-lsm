9d475cf3c278cd47283b1ace5b63986e81302bf4
Vadim Ryabov <mr.holeyko@gmail.com>
2023-11-23 16:56:58 +0300

    Improved code

--- src/main/java/ru/vk/itmo/test/ryabovvadim/sstable/SSTable.java
+++ src/main/java/ru/vk/itmo/test/ryabovvadim/sstable/SSTable.java
@@ -19,6 +19,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.READ;
@@ -242,5 +243,39 @@ public class SSTable {
         }
     }
 
-    private record RecordInfo(byte meta, long keySize, long keyOffset, long valueSize, long valueOffset) { }
+    private static final class RecordInfo {
+        private final byte meta;
+        private final long keySize;
+        private final long keyOffset;
+        private final long valueSize;
+        private final long valueOffset;
+
+        private RecordInfo(byte meta, long keySize, long keyOffset, long valueSize, long valueOffset) {
+            this.meta = meta;
+            this.keySize = keySize;
+            this.keyOffset = keyOffset;
+            this.valueSize = valueSize;
+            this.valueOffset = valueOffset;
+        }
+
+        public byte meta() {
+            return meta;
+        }
+
+        public long keySize() {
+            return keySize;
+        }
+
+        public long keyOffset() {
+            return keyOffset;
+        }
+
+        public long valueSize() {
+            return valueSize;
+        }
+
+        public long valueOffset() {
+            return valueOffset;
+        }
+    }
 }
