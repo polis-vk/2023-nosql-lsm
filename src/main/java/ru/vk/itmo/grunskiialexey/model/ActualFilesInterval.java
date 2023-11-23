65e6317eb4c2abbd29c278d87a759cfb9186950f
MegaVerkruzo <robochat@mail.ru>
2023-11-23 23:58:29 +0300

    Save without import

--- src/main/java/ru/vk/itmo/grunskiialexey/model/ActualFilesInterval.java
+++ src/main/java/ru/vk/itmo/grunskiialexey/model/ActualFilesInterval.java
@@ -1,4 +1,9 @@
 package ru.vk.itmo.grunskiialexey.model;
 
+/*
+    I save in index.idx only positions in interval [a, b) of actual ss-tables.
+    If you see 0, 0 => then you have no ss-tables;
+    If you see 2, 5 => then you have "2", "3", "4" ss-tables
+ */
 public record ActualFilesInterval(long left, long right) {
 }
