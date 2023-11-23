f7557210f0e1abedd490174bee2691c0aa84dab4
Vadim Ryabov <mr.holeyko@gmail.com>
2023-11-23 18:21:19 +0300

    Improved code

--- src/main/java/ru/vk/itmo/test/ryabovvadim/memtable/MemoryTable.java
+++ src/main/java/ru/vk/itmo/test/ryabovvadim/memtable/MemoryTable.java
@@ -2,6 +2,7 @@ package ru.vk.itmo.test.ryabovvadim.memtable;
 
 import ru.vk.itmo.Entry;
 import ru.vk.itmo.test.ryabovvadim.exception.MemoryTableOutOfMemoryException;
+import ru.vk.itmo.test.ryabovvadim.iterators.MemoryMergeIterators;
 import ru.vk.itmo.test.ryabovvadim.sstable.SSTableManager;
 import ru.vk.itmo.test.ryabovvadim.utils.IteratorUtils;
 import ru.vk.itmo.test.ryabovvadim.utils.MemorySegmentUtils;
@@ -50,7 +51,7 @@ public class MemoryTable {
     }
 
     public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
-        return mergeIterators(
+        return new MemoryMergeIterators(
                 getIterator(memTable, from, to),
                 getIterator(flushTable, from, to)
         );
@@ -73,76 +74,6 @@ public class MemoryTable {
             return table.tailMap(from).values().iterator();
         }
         return table.subMap(from, to).values().iterator();
-    }
-
-    private Iterator<Entry<MemorySegment>> mergeIterators(
-            Iterator<Entry<MemorySegment>> fstIterator,
-            Iterator<Entry<MemorySegment>> sndIterator
-    ) {
-        return new Iterator<>() {
-            private Entry<MemorySegment> fstEntry;
-            private Entry<MemorySegment> sndEntry;
-
-            @Override
-            public boolean hasNext() {
-                return fstIterator.hasNext()
-                        || sndIterator.hasNext()
-                        || fstEntry != null
-                        || sndEntry != null;
-            }
-
-            @Override
-            public Entry<MemorySegment> next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                getFstNext();
-                getSndNext();
-
-                Entry<MemorySegment> result;
-                if (fstEntry == null) {
-                    result = sndEntry;
-                    sndEntry = null;
-                } else if (sndEntry == null) {
-                    result = fstEntry;
-                    fstEntry = null;
-                } else {
-                    result = compare();
-                }
-
-                return result;
-            }
-
-            private void getFstNext() {
-                if (fstEntry == null && fstIterator.hasNext()) {
-                    fstEntry = fstIterator.next();
-                }
-            }
-
-            private void getSndNext() {
-                if (sndEntry == null && sndIterator.hasNext()) {
-                    sndEntry = sndIterator.next();
-                }
-            }
-
-            private Entry<MemorySegment> compare() {
-                int compareResult = MemorySegmentUtils.compareMemorySegments(fstEntry.key(), sndEntry.key());
-                Entry<MemorySegment> result;
-                if (compareResult < 0) {
-                    result = fstEntry;
-                    fstEntry = null;
-                } else if (compareResult == 0) {
-                    result = fstEntry;
-                    fstEntry = null;
-                    sndEntry = null;
-                } else {
-                    result = sndEntry;
-                    sndEntry = null;
-                }
-
-                return result;
-            }
-        };
     }
 
     public void upsert(Entry<MemorySegment> entry) {
